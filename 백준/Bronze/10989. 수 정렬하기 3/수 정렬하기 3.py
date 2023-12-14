28c306daccabc876f0e62c4db807cefb6e45c31e
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-14 10:42:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9372 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,16 @@
+# 이 문제는 수의 개수가 많고 각 숫자의 크기가 크지 않다.
+# 또한 메모리 제한이 8MB이기 때문에 메모리 사용에 주의해야 한다.
+# for문 속에서 append를 사용하게 되면
+# 메모리 재할당이 이루어져서 메모리를 효율적으로 사용못한다.
+import sys
+
+# 미리 최대 숫자 크기의 list를 만드는 방법 사용
+N = int(sys.stdin.readline())
+num_list = [0] * 10001
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    num_list[num]+=1
+
+for i in range(len(num_list)):
+    for _ in range(num_list[i]):
+        print(i)
