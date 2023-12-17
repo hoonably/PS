3ad46542d091ba7b0c07acb609fe7001432d4e6f
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-17 22:12:04 +0900

    [Platinum V] Title: 버블 소트, Time: 708 ms, Memory: 232132 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.py
+++ 백준/Platinum/1517. 버블 소트/버블 소트.py
@@ -4,8 +4,10 @@ N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 answer = 0
 
-def merge_sort(arr):
+# 이 문제는 제목은 버블소트이지만 시간복잡도때문에 병합정렬을 이용해서 버블소트때 스왑한 횟수를 구하면 된다.
 
+
+def merge_sort(arr):
     global answer
 
     if len(arr) < 2:
@@ -22,12 +24,14 @@ def merge_sort(arr):
     merged_index = 0
 
     while l < len(low_arr) and h < len(high_arr):
+        # 값이 같을때는 스왑을 할 필요가 없기 때문에 <= 을 붙여줘야한다.
         if low_arr[l] <= high_arr[h]:
             merged_arr.append(low_arr[l])
             merged_index += 1
             l += 1
         else:
             merged_arr.append(high_arr[h])
+            # 이거만 추가해주면 된다.
             answer += len(low_arr) + h - merged_index
             merged_index += 1
             h += 1
