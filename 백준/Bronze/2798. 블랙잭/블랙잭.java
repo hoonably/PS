8ccde9766ebb1fdc9c09419efb8ca8017e044dd8
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-18 18:44:07 +0900

    [Bronze II] Title: 블랙잭, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		finding(arr,n, m);
+	}
+	
+	static void finding(int[] arr, int n, int m) {
+		int best =0;
+		for(int i=0;i<n-2;i++) {
+			for(int j=i+1;j<n-1;j++) {
+				for(int k=j+1;k<n;k++) {
+					int temp = arr[i]+arr[j]+arr[k];
+					if(temp==m) {
+						System.out.println(temp);
+						return;
+					}
+					if(temp>best&&temp<m) {
+						best=temp;
+					}
+				}
+			}
+		}
+		System.out.println(best);
+	}
+}
