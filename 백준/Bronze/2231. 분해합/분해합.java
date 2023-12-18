70ad8b5e62c28ecf9332b5b5c12b351d52365fb9
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-18 18:34:09 +0900

    [Bronze II] Title: 분해합, Time: 348 ms, Memory: 65592 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		System.out.println(answer(n));
+	}
+	
+	static int answer(int n) {
+		for(int i=0;i<n;i++) {
+			int temp=0;
+			String istr = Integer.toString(i);
+			for(int j=0;j<istr.length();j++) {
+				temp+=istr.charAt(j)-'0';
+				}
+			if(temp+i==n) {
+				return i;
+			}
+		}
+		return 0;
+	}
+}
