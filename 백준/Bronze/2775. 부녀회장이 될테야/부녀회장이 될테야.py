d7de2cf9cf19b46a1883fa6acc1b8271a8d36b05
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-18 18:43:59 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,16 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    k = int(sys.stdin.readline())      # 층
+    n = int(sys.stdin.readline())      # 호
+    people = [i for i in range(1, n+1)]     # 0층
+
+    for x in range(k):
+        new = []
+        for y in range(n):
+            new.append(sum(people[:y+1]))   # 아래층의 1~n호 까지의 합
+        people = new.copy()
+        #print(people)		# peaple에 들어있는 값 출력해 봄
+    print(people[-1])       # K층 n호
