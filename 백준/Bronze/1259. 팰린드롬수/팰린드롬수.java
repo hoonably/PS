324f966b2fe0bf6cd2589adfb1f553c855411c89
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-18 18:23:57 +0900

    [Bronze I] Title: 팰린드롬수, Time: 204 ms, Memory: 17408 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String str = sc.next();
+			if(str.equals("0")) {
+				break;
+			}
+	        StringBuffer sb = new StringBuffer(str);        
+	        String reverse = sb.reverse().toString();
+	        if(str.equals(reverse)) {
+	        	System.out.println("yes");
+	        }
+	        else {
+	        	System.out.println("no");
+	        }
+		}
+		sc.close();
+		
+	}
+
+}
