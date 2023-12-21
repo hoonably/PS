4050848a9b9881b03155e886b823cde33b72c3f7
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-21 11:33:48 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 200 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		StringBuffer sb = new StringBuffer(str);
+		String reverse = sb.reverse().toString();
+		
+		if(str.equals(reverse)) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(0);
+		}
+
+
+	}
+
+}
