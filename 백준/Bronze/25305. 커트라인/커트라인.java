fd149fdc7736faa20ee22ea3daf80e810d8fdda9
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-21 11:49:53 +0900

    [Bronze II] Title: 커트라인, Time: 296 ms, Memory: 20764 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int k = sc.nextInt();
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		bubblesort(arr);
+		System.out.println(arr[n-k]);
+		
+	}
+	public static void bubblesort(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0; j < arr.length - i - 1 ; j++) {
+				if(arr[j]>arr[j+1]) {
+					swap(arr,j,j+1);
+				}
+			}
+		}
+	}
+	
+	public static void swap (int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+
+}
