cfc283cb731fd4036473b94433cf6b4430b6fef9
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-29 19:06:46 +0900

    [Silver III] Title: N과 M (8), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,29 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+nums = list(map(int, sys.stdin.readline().split()))
+nums.sort()
+
+arr = []
+
+def backtracking(n):
+    # 갯수 완성했으면 출력
+    if n == M:
+        print(*arr)
+        return
+
+    #모든 자식 노드에 대해
+    for i in range(N):
+
+        # 뒤에가 더 작은거 방지
+        if arr and nums[i] < arr[-1]:
+            continue
+
+        arr.append(nums[i])
+        # 유망하면 자식노드로 이동
+        backtracking(n+1)
+        # 백트래킹 다 끝나면 부모노드로 이동
+        arr.pop()
+
+backtracking(0)
