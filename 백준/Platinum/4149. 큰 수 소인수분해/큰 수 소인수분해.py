665287eb87d1434635e5f381f2d8cde58edf2ee2
hhhoon <77783081+hhhoon@users.noreply.github.com>
2023-12-30 20:22:40 +0900

    [Platinum I] Title: 큰 수 소인수분해, Time: 312 ms, Memory: 39912 KB, Score: 161 point -BaekjoonHub

--- 백준/Platinum/4149. 큰 수 소인수분해/큰 수 소인수분해.py
+++ 백준/Platinum/4149. 큰 수 소인수분해/큰 수 소인수분해.py
@@ -1,3 +1,5 @@
+# https://psforhobby.tistory.com/10
+
 import random
 
 def miller_rabin(n, k=5):
@@ -26,7 +28,7 @@ def miller_rabin(n, k=5):
             return False
     return True
 
-def pollards_rho(n, attempts=50):
+def pollards_rho(n, attempts=70):
     for _ in range(attempts):
         x = random.randint(1, n - 1)
         y = x
