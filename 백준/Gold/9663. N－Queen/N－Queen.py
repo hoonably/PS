96ce0c7eca589c825afa51cfaab540853bbb2440
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-01 22:38:01 +0900

    [Gold IV] Title: N-Queen, Time: 23024 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,49 +1,30 @@
-# 첫번째 열에 1을 놓을때 -> 첫번째 열에 N을 놓을 때까지 backtracking 알고리즘을 사용한다.
-'''
-모든 경우는 좌우 대칭이다. -> 아직 안해줌
-0 1 0 0
-0 0 0 1
-1 0 0 0
-0 0 1 0
+N = int(input())
+cnt = 0
+row = [True]*N # 가로
+x1 = [True]*(2*N) # 제일 왼쪽이 인덱스인 우상향 대각선
+x2 = [True]*(2*N) # 제일 오른쪽이 인덱스인 우하향 대각선
 
-0 0 1 0
-1 0 0 0
-0 0 0 1
-0 1 0 0
-'''
-
-def q_check(x):
-    # 이미 기록된 0 ~ X-1 범위 보기
-    for i in range(x):
-        # row (가로)체크
-        # 체크하려는 row == 기록된 row(q_list[i]) 일때 False
-        if row[x] == row[i]:
-            return False
-        # 대각선 체크
-        # 체크하려는 row 와 이미 기록된 row 의 차가
-        # 체크하려는 column 과 기록된 column 의 차랑 같다면 대각선
-        # abs() = 절댓값 함수
-        if abs(row[x]-row[i]) == x-i:
-            return False
-    # 두 경우 다 피해가면 True
-    return True
-
-
-def n_queen(c):
-    global answer
-
-    # 끝까지 완성한 경우
-    if c==N:
-        answer+=1
+def backtracking(n):
+    global cnt
+    # 다 도달했을 때 1카운트 후 종료
+    if n==N:
+        cnt+=1
         return
 
     for i in range(N):
-        row[c] = i
-        if q_check(c):
-            n_queen(c+1)
+        # 놓을 수 있으면
+        if row[i] and x1[i+n] and x2[i+((N-1)-n)]:
+            row[i]=False # 가로줄 제거
+            x1[i+n]=False # 오른쪽 위 방향 대각선 제거
+            x2[i+((N-1)-n)]=False # 오른쪽 아래 방향 대각선 제거
 
-N = int(input())
-row = [0]*N
-answer = 0
-n_queen(0)
-print(answer)
+            # 자식 노드로 이동
+            backtracking(n+1)
+
+            # 백트래킹
+            row[i]=True
+            x1[i+n]=True
+            x2[i+((N-1)-n)]=True
+
+backtracking(0)
+print(cnt)
