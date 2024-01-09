cc2b0224fd70f89b015ae0e46887a41ef17064e9
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-09 18:56:02 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,14 +1,13 @@
+# 분할정복 거듭제곱
 import sys
 
+def power(a,n):
+    if n==1:
+        return a % C
+    elif n%2==1:
+        return power(a, n-1) * a % C
+    else:
+        return (power(a, n//2) ** 2) % C
+
 A, B, C = map(int, sys.stdin.readline().split())
-
-# a의 b승을 c로 나누는 함수
-def div_mul(a,b,c):
-    if b==1:
-        return a%c
-    if b%2==0: # 제곱 차수가 짝수인 경우
-        return (div_mul(a,b//2,c)**2) % c
-    else: # 제곱 차수가 홀수인 경우
-        return (div_mul(a,b//2+1,c)*div_mul(a,b//2,c)) % c
-
-print(div_mul(A,B,C))
+print(power(A, B))
