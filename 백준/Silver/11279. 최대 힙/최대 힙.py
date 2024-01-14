2b378309041b7319e4fbccfb0128ade776d8d7b3
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-14 16:55:07 +0900

    [Silver II] Title: 최대 힙, Time: 124 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -5,12 +5,12 @@ N = int(sys.stdin.readline())
 heap1 = []
 for _ in range(N):
     get_num = int(sys.stdin.readline())
-    if get_num == 0:
+    if get_num != 0:
+        # push할때 -를 곱한다.
+        heapq.heappush(heap1, -get_num)
+    else:
         if len(heap1)==0:
             print(0)
         else:
             # pop할때도 -를 곱한다.
             print(-heapq.heappop(heap1))
-    else:
-        # push할때 -를 곱한다.
-        heapq.heappush(heap1, -get_num)
