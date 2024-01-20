2f99d756cd0e7c8cdfabccff43ac1c37736329fa
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-20 15:24:34 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("|\\_/|");  printf("\n");
+    printf("|q p|   /}");   printf("\n");
+    printf("( 0 )\"\"\"\\");    printf("\n");
+    printf("|\"^\"`    |");   printf("\n");
+    printf("||_/=\\\\__|");   printf("\n");
+    return 0;
+}
