709e9ec01a00b837b420a6a105264501c6328b5f
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-20 15:38:37 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    int A, B;
+    for (int i=0; i<N; i++){
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A+B);
+    }
+    return 0;
+}
