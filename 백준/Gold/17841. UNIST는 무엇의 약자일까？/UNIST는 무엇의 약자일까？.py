b704136dafb2ddd575c3daef8b4ce854d78a68c0
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-20 21:33:47 +0900

    [Gold III] Title: UNIST는 무엇의 약자일까?, Time: 588 ms, Memory: 51116 KB -BaekjoonHub

--- 백준/Gold/17841. UNIST는 무엇의 약자일까？/UNIST는 무엇의 약자일까？.py
+++ 백준/Gold/17841. UNIST는 무엇의 약자일까？/UNIST는 무엇의 약자일까？.py
@@ -3,16 +3,23 @@
 import sys
 
 n = int(sys.stdin.readline())
-P = [''] * n
-# DP[i][j] : i번 문자열부터 UNIST의 j번째 부터 만드는 경우의 수
-# DP[ ][5] = 1 : DP 탑다운을 위해서 설정
-DP = [[0,0,0,0,0,1] for _ in range(n+1)]
+m=n
+P = []
 
 for i in range(n):
     s = sys.stdin.readline().rstrip()
-    P[i] = s[:5]  # 어차피 5글자 이후로는 필요가 없음
 
-for i in reversed(range(n)): # n-1 -> 0
+    # 미리 필요없는거 넣지 않기
+    if not s[0] in "UNIST":
+        m-=1
+        continue
+    P.append(s[:5])  # 어차피 5글자 이후로는 필요가 없음
+
+# DP[i][j] : i번 문자열부터 UNIST의 j번째 부터 만드는 경우의 수
+# DP[ ][5] = 1 : DP 탑다운을 위해서 설정
+DP = [[0,0,0,0,0,1] for _ in range(m+1)]
+
+for i in reversed(range(m)): # n-1 -> 0
     for j in reversed(range(5)): # 4-> 0
         DP[i][j] = DP[i + 1][j]
         for k in range(5 - j):
