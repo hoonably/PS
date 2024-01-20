ee1f9e3f214cd491f24c5b42493df36a0edc306c
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-21 00:04:15 +0900

    [Platinum IV] Title: Four XOR, Time: 504 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/21099. Four XOR/Four XOR.py
@@ -0,0 +1,17 @@
+n = int(input())
+if n > 60:
+    print("Yes")
+    exit()
+else:
+    input_list = list(map(int, input().split()))
+    result = input_list[0]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                for m in range(n):
+                    result = (input_list[i] ^ input_list[j] ^ input_list[k] ^ input_list[m])
+                    if result == 0:
+                        if input_list[i] < input_list[j] < input_list[k] < input_list[m]:
+                            print("Yes")
+                            exit()
+print("No")
