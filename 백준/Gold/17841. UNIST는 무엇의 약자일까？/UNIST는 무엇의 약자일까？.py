8ae106fbb373307f0893a2f946c6c79620e447a7
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-20 22:30:49 +0900

    [Gold III] Title: UNIST는 무엇의 약자일까?, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17841. UNIST는 무엇의 약자일까？/UNIST는 무엇의 약자일까？.py
+++ 백준/Gold/17841. UNIST는 무엇의 약자일까？/UNIST는 무엇의 약자일까？.py
@@ -10,10 +10,9 @@ for i in range(5):
 def compare(x):
     # 검사할 범위
     length = min(5, len(x))
-    for l in reversed(range(1, length+1)): # 최대 5부터 1까지 검사
-        if x[:l] in correct[l]: # 앞에서부터 l개 커팅한게 집합에 있는지
-            # 맞은 개수만큼 리턴
-            return l
+    for l in reversed(range(1, length+1)):  # 최대 5부터 1까지 검사
+        if x[:l] in correct[l]:  # 앞에서부터 l개 커팅한게 집합에 있는지
+            return l  # 맞은 개수만큼 리턴
     return 0
 
 dp = [0] * 6
@@ -23,16 +22,16 @@ N = int(sys.stdin.readline())
 
 for _ in range(N):
     st = sys.stdin.readline().rstrip()
-    if not st[0] in 'UNIST': # 불필요한 단어 미리 삭제
+    if not st[0] in 'UNIST':  # 불필요한 단어 미리 삭제
         continue
 
     # 시작지점 : U:1, N:2 이런식으로
     start = 'UNIST'.index(st[0]) + 1
 
     # 맞는 길이
-    l = compare(st)
+    corlen = compare(st)
 
-    for i in range(start, start + l):
+    for i in range(start, start + corlen):
         dp[i] += dp[start - 1]
         dp[i] %= 1000000007
 
