9e577e365c2bd78adf436b3c5ad9151c0381dd3c
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-20 15:22:09 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    printf("\\    /\\");  printf("\n");
+    printf(" )  ( ')");   printf("\n");
+    printf("(  /  )");    printf("\n");
+    printf(" \\(__)|");   printf("\n");
+    return 0;
+}
