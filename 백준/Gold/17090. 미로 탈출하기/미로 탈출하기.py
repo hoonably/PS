8fb9bded2c6400a981e5d9b471fd50c538b122ac
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-23 11:32:04 +0900

    [Gold III] Title: 미로 탈출하기, Time: 392 ms, Memory: 89028 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.py
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.py
@@ -8,17 +8,17 @@ def dfs(x,y,depth):
     if x==-1 or y==-1 or x==N or y==M:  # 탈출
         ans+=depth
         return True
-    if board[x][y] == 'T':  # 탈출가능했던 경로 나오면
+    if board[x][y] == 'T':  # 탈출 가능했던 경로 나오면
         ans+=depth
         return True
-    if board[x][y] == 'F':  # 탈출불가능했던 경로 나오면
+    if board[x][y] == 'F':  # 탈출 불가능했던 경로 or 현재 dfs경로 나오면
         return False
 
     # if visited[x][y]: # 이전꺼와는 안만나지만 지금이 무한반복이라면
     #     return False
 
     dx, dy = dic[board[x][y]]
-    board[x][y] = 'F'  # 지금 상황에서 무한반복 방지
+    board[x][y] = 'F'  # 현재 dfs경로는 일단 F로 기록 (성공하면 나중에 T로 바뀜)
     result = dfs(x+dx, y+dy, depth+1)  # 다음 진행상황의 탈출 여부
     board[x][y] = ('T' if result else 'F')  # 다음 진행상황이 True라면 얘도 'T'로 기록
 
