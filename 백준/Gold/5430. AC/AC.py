2dc163c6a16fe2df1e68fc8c753908eddc45dd83
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-25 23:42:07 +0900

    [Gold V] Title: AC, Time: 640 ms, Memory: 39164 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+T = int(input())
+for _ in range(T):
+    p = sys.stdin.readline().rstrip()
+    n = int(input())
+    x = sys.stdin.readline().rstrip()
+    if x == '[]':
+        x = deque()
+    else:
+        x = deque(map(int, x[1:-1].split(',')))
+
+    check = True
+    reverse_check = 0
+    for do in p:
+        if do == 'R':
+            reverse_check ^= 1
+
+        elif len(x)==0:  # D 하려는데 배열이 비어있다면
+            check = False
+            print('error')
+            break
+
+        elif reverse_check:  # 홀수번 뒤집혔다면
+            x.pop()
+        else:  # 짝수번 뒤집혔다면
+            x.popleft()
+
+    if check:
+        if reverse_check:  # 홀수번 R이 있었다면
+            x.reverse()  # deque 뒤집기
+
+        if len(x)==0:
+            print('[]')
+        else:
+            print('[', end='')
+            for i in range(len(x)-1):
+                print(x[i], end=',')
+            print(x[-1], ']', sep='')
