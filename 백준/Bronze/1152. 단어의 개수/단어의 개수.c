063a50a7426492c83cc3274dedacd2e250c4f6c2
hoonably <77783081+hoonably@users.noreply.github.com>
2024-01-30 11:07:24 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 1968 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char word[1000000];
+    int count = 0, len;
+    
+    // 엔터 입력할때까지 scanf 받기
+    scanf("%[^\n]s", word);
+    len = strlen(word);
+    
+    // 띄어쓰기만 있을 경우 0 출력
+    if (len == 1 && word[0] == ' ') {
+        printf("%d", count);
+        return 0;
+    }
+    
+    for (int i = 1; i < len - 1; i++) {
+        if (word[i] == ' ') {
+            count++;
+        }
+    }
+    printf("%d", count + 1);
+    
+    return 0;
+}
