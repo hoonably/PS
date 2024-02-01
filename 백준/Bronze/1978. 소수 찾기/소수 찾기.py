2ebe505339714566f6aedd0a0dba861d650373c5
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-01 16:02:07 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -5,13 +5,15 @@ numbers = list(map(int, sys.stdin.readline().split()))
 
 answer = 0 # 소수 개수
 for i in numbers:
+    #1은 소수가 아니므로 건너 뜀
+    if i==1:
+        continue
+
     sosu = True
     for j in range(2,i//2+1): # 2부터 n/2 까지 확인
         if i%j==0:
             sosu = False
             break
-    if i==1:
-        sosu = False
     if sosu:
         answer+=1
 print(answer)
