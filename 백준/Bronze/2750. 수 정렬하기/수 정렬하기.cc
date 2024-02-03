2aee32cde70609ddea75db39c5c60a2d5312cef9
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-03 14:55:43 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,34 +1,23 @@
-// 인접한 두 원소를 검사하여 순서가 맞지 않으면 교환
-// 시간 복잡도 : 최선 O(N^2) / 평균 O(N^2) / 최악 O(N^2)
-
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int arr[1000];
+int num[1000]; 
 
-void bubble_sort(int *arr, int n){
-    int i, j, temp;
+int main() {
+	int N;
+	cin >> N;
 
-    for (i=n-1; i>0; i--) {
-        for (j=0; j<i; j++) {
-            if (arr[j]>arr[j+1]) {
-            temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-            }
-        }
-    }
-}
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
 
-int main(void){
-    int n;
-    cin >> n;
-    for (int i=0; i<n; i++){
-        cin >> arr[i];
-    }
-    bubble_sort(arr, n);
-    for (int i=0; i<n; i++){
-        cout << arr[i] << "\n";
-    }
-    return 0;
+    // sort(시작 배열 주소, 끝 배열 주소)
+	sort(num, num + N);
+
+	for (int i = 0; i < N; i++) {
+		cout << num[i] << "\n";
+	}
+
+	return 0;
 }
