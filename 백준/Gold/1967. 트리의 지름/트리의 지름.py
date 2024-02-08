f23b52fefbe43b1ca690e060d8d413b262d1c56a
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-08 22:13:37 +0900

    [Gold IV] Title: 트리의 지름, Time: 152 ms, Memory: 244408 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,8 +1,16 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**5)
+
+# 이번엔 dfs로 구현
+def dfs(node, cost):
+    for next_node, next_dist in tree[node]:
+        next_dist += cost
+        if visited[next_node] == -1:
+            visited[next_node] = next_dist  # 방문 표시를 거리로
+            dfs(next_node, next_dist)
+    return
 
 N = int(sys.stdin.readline())
-visited = [False]*(N+1)
 tree = [[] for _ in range(N+1)]
 
 for _ in range(N-1):
@@ -10,34 +18,26 @@ for _ in range(N-1):
     tree[par].append((son, dist))
     tree[son].append((par, dist))
 
-# start_node로부터 가장 먼 노드와 거리를 뽑아내는 함수
-def bfs(start_node):
-    queue = deque()
-    queue.append((start_node, 0))
-    visited = [False] * (N + 1)
-    visited[start_node] = True
-    diameter_of_tree = 0  # start node 로부터 가장 먼 거리
-    diameter_point = 0  # start node 로부터 가장 먼 노드
-
-    while queue:
-        now_node, now_dist = queue.popleft()
-        for next_node, next_dist in tree[now_node]:
-            if not visited[next_node]:
-                next_dist += now_dist
-                queue.append((next_node, next_dist))
-                visited[next_node] = True
-
-                # 가장 먼 노드와 그 길이 갱신
-                if diameter_of_tree < next_dist:
-                    diameter_point = next_node
-                    diameter_of_tree = next_dist
-
-    return diameter_of_tree, diameter_point
-
 # 트리의 지름 : 트리에서 임의의 두 점 사이의 거리 중 가장 긴 것
 # 트리의 지름을 구하는 방법
 # 임의의 점 (1) 부터 가장 먼 거리의 노드 (point) 를 구한다.
 # 그 후 그 point에서 가장 먼 거리로 한번 더 탐색을 하면 그게 최대인 지름이다.
 
-point = bfs(1)[1]
-print(bfs(point)[0])
+visited = [-1]*(N+1)
+visited[1] = True
+dfs(1, 0)
+best_idx, best_dist = 0, 0
+for i in range(2, N+1):
+    if visited[i] > best_dist:
+        best_idx = i
+        best_dist = visited[i]
+
+# 다시 한번 더
+visited = [-1]*(N+1)
+visited[best_idx] = 0
+dfs(best_idx, 0)
+
+print(max(visited))
+
+
+
