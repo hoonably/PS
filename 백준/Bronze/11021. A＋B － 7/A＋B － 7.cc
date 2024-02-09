3e7c7aa36ed87cb6405d54677e3b624dfde0cbc7
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-09 18:05:12 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -4,12 +4,12 @@ using namespace std;
 int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int T, A, B;
     cin >> T;
     for (int i=1; i<=T; i++){
         cin >> A >> B;
-        cout << "Case #" << i << ": " << A+B << endl;
+        cout << "Case #" << i << ": " << A+B << '\n';
     }
     return 0;
 }
