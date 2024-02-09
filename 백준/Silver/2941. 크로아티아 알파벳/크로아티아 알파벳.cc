c80aa1095204fcb1808d8f5fe62d032f08e86943
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-09 20:49:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    string word;
+    int cnt=0;
+    cin >> word;
+
+    for (int i=0; i<word.length(); i++){
+        if (word[i]=='c'){
+            if (word[i+1]=='=' || word[i+1]=='-'){
+                i++;
+            }
+        }
+        else if (word[i]=='d'){
+            if (word[i+1]=='z' && word[i+2]=='='){
+                i+=2;
+            }
+            else if (word[i+1]=='-'){
+                i++;
+            }
+        }
+        else if (word[i]=='l'){
+            if (word[i+1]=='j'){
+                i++;
+            }
+        }
+        else if (word[i]=='n'){
+            if (word[i+1]=='j'){
+                i++;
+            }
+        }
+        else if (word[i]=='s'){
+            if (word[i+1]=='='){
+                i++;
+            }
+        }
+        else if (word[i]=='z'){
+            if (word[i+1]=='='){
+                i++;
+            }
+        }
+        cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
