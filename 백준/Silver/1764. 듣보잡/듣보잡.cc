4bf7d85e8ab1857af8fc038e4a2a3396a4b65253
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-11 04:53:14 +0900

    [Silver IV] Title: 듣보잡, Time: 44 ms, Memory: 7572 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M;
+    string name;
+    set<string> not_heard, not_both;
+    cin >> N >> M;
+    for (int i=0; i<N; i++){
+        cin >> name;
+        not_heard.insert(name);
+    }
+    for (int i=0; i<N; i++){
+        cin >> name;
+        if (not_heard.find(name)!=not_heard.end()){
+            not_both.insert(name);
+        }
+    }
+    cout << not_both.size() << '\n';
+    for (string n : not_both){
+        cout << n << '\n';
+    }
+    
+    return 0;
+}
