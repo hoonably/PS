c37c70ada1ac4c7899220be31ea4c533142a6cbb
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-10 18:21:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -4,14 +4,17 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
     vector<pair<int, int>> v;
 	int N;
 	cin >> N;
 	int x, y;
 	for (int i = 0; i < N; i++) {
 		cin >> x >> y;
-		v.push_back(make_pair(x, y));
-        // v.push_back({ x, y }); 랑 같음
+		// v.push_back(make_pair(x, y));
+        v.push_back({ x, y });
 	}
 	sort(v.begin(), v.end());
 	for (int i = 0; i < N; i++) {
