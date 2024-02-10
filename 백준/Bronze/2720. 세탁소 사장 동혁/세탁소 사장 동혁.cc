af1aae26bb95a6db47d8878a2457ded9b1b3385e
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-10 15:57:01 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     int T;
     cin >> T;
     for(int i=0;i<T;i++) {
         int C;
         cin >> C;
-        cout << C/25 << " ";
+        cout << C/25 << ' ';
         C%=25;
-        cout << C/10 << " ";
+        cout << C/10 << ' ';
         C%=10;
-        cout << C/5 << " ";
+        cout << C/5 << ' ';
         C%=5;
-        cout << C << endl;
+        cout << C << '\n';
     }
     return 0;
 }
