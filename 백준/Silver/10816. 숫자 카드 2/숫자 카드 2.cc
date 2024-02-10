534b133c9b4223b316faba5cecb550374a73fd0a
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-10 23:25:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,34 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
+int n, m, x;
+int arr[500001];
+int main(void) {
+    ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int N, M, get_num;
-    // map 선언
-    map<int, int> card;
-    
-    cin >> N;
-    for (int i=0; i<N; i++){
-        cin >> get_num;
-        // map에 없다면
-        if(card.find(get_num) == card.end()){
-            card[get_num] = 1;
-        }
-        else{
-            card[get_num]++;  // 있다면 1 올리기
-        }
-        
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+    cin >> m;
+    while(m--) {
+        cin >> x;
+        cout << upper_bound(arr, arr+n, x) - lower_bound(arr, arr+n, x) << ' ';
     }
-    cin >> M;
-    for (int i=0; i<M; i++){
-        cin >> get_num;
-        if (card.find(get_num) == card.end()){
-            cout << "0 ";
-        }
-        else cout << card[get_num] << ' ';
-    }
-
-    return 0;
 }
