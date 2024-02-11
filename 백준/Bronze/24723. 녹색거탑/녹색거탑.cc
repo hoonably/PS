66b32054844de97d938ebddfab3392ae1cade397
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-12 01:46:11 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    cout << pow(2, N) << endl;
+    
+    return 0;
+}
