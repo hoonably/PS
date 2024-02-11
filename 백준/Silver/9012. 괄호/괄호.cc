f2636a1d102a6c40b189a26282fa4a1f451b3795
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-11 19:15:21 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(const string& str) {
+    stack<char> stack;
+    for (char s : str) {
+        if (s == '(') {
+            stack.push(0);
+        } else if (s == ')') {
+            if (stack.empty()) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+    }
+    return stack.empty();  // 비어있다면 true 아니면 false
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    string str;
+    cin >> T;
+    while (T--){
+        cin >> str;
+        if (check(str)) cout << "YES" << '\n';
+        else cout << "NO" << '\n';
+    }
+    
+    return 0;
+}
