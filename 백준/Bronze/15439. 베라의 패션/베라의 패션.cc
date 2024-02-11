6d7fd7dd7d935def703c35cdd8c7877ee7f549f4
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-12 01:44:33 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    cout << N*(N-1) << endl;
+    
+    return 0;
+}
