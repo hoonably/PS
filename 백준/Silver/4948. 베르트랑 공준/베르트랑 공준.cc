5d6ef67eccfedd51b60d49260cecee9c61725eb8
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-11 17:29:02 +0900

    [Silver II] Title: 베르트랑 공준, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,46 @@
+// 에라토스테네스의 체로 N까지 소수가 맞는지 bool 벡터 얻어오기
+// O(N^1/2)
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> findPrimes(int N) {
+
+    // 소수 여부를 나타내는 배열
+    vector<bool> isPrime(N + 1, true);
+    isPrime[0] = false;  // 0은 소수가 아님 (실수 방지)
+    isPrime[1] = false;  // 1은 소수가 아님
+
+    // 에라토스테네스의 체 알고리즘 적용
+    for (int i = 2; i * i <= N; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime;
+}
+
+int main() {
+
+	int n;
+    while(true){
+        cin >> n;
+        if (n==0) break;
+
+        // 123456 * 2 이하의 자연수가 각각 소수인지 나타내는 bool 배열 얻어오기
+        vector<bool> isPrime = findPrimes(123456 * 2);
+
+        int cnt=0;
+        for (int i=n+1; i<=2*n; i++){
+            if (isPrime[i]) cnt++;
+        }
+        cout << cnt << '\n';
+    }
+
+
+    return 0;
+}
