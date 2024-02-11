f3aadd3ac0de97e018842dfc73d71975d57957f1
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-11 18:37:16 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 16 ms, Memory: 3164 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -1,43 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> primes; // 소수 벡터
+vector<int> primes;
 
-vector<int> findPrimes(int N) {
+vector<bool> Eratos(int N) {
 
     // 소수 여부를 나타내는 배열
-    vector<int> isPrime(N + 1, -1);
-    // -1 : 아직 모르거나 2가 아닌 짝수
-    // 0  : 합성수 
-    // 1  : 소수
+    vector<bool> isPrime(N + 1, true);
+    isPrime[0] = false;  // 0은 소수가 아님 (실수 방지)
+    isPrime[1] = false;  // 1은 소수가 아님
 
-    isPrime[2] = true;  // 2는 유일한 짝수 소수
-    primes.emplace_back(2);
-
-
-    // 에라토스테네스의 체 알고리즘 안씀 (소수 벡터 만들어야하기 때문)
-    for (int i = 3; i <= N; i+=2) {
-
-        // 합성수 표기가 안된 홀수라면 소수임
-        if (isPrime[i] == -1) {
-            isPrime[i] = 1; // 소수 표시
-            primes.emplace_back(i); // 소수 벡터에 추가
-
-            // 배수는 모두 합성수 표시
+    // 에라토스테네스의 체 알고리즘 적용
+    for (int i = 2; i * i <= N; i++) {
+        if (isPrime[i]) {
             for (int j = 2 * i; j <= N; j += i) {
-                isPrime[j] = 0;
+                isPrime[j] = false;
             }
         }
     }
+
     return isPrime;
 }
+
+// n 까지의 소수 벡터 만들기
+void make_vector_prime(int n, vector<bool> isPrime) {
+	for (int i = 2; i <= n; ++i)
+		if (isPrime[i]) primes.push_back(i);
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
-    // 1000000 이하의 자연수가 각각 소수인지 나타내는 int 배열 얻어오기
-    vector<int> isPrime = findPrimes(1000000);
+    // 1000000 이하의 자연수가 각각 소수인지 나타내는 bool 배열 얻어오기
+    vector<bool> isPrime = Eratos(1000000);
+    make_vector_prime(1000000, isPrime);
 
     int T;
     cin >> T;
