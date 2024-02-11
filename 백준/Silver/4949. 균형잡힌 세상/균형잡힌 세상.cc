aae44a3036f38da81bab89b39ba904841f85bfa3
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-11 19:47:04 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(const string& str) {
+    stack<int> stack;
+    for (char s : str) {
+        if (s == '(' || s == '[') {
+            stack.push(s);
+        }
+        else if (s == ')') {
+            if (stack.empty()) return false;
+            else if (stack.top() == '(') stack.pop();
+            else return false;
+        }
+        else if (s == ']') {
+            if (stack.empty()) return false;
+            else if (stack.top() == '[') stack.pop();
+            else return false;
+        }
+    }
+    return stack.empty();  // 비어있다면 true 아니면 false
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    string str;
+    while (true){
+        getline(cin, str);
+        if (str == ".") break;
+
+        if (check(str)) cout << "yes" << '\n';
+        else cout << "no" << '\n';
+    }
+    
+    return 0;
+}
