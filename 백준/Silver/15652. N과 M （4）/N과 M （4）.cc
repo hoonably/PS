21c5c6daad1f673dd07d7f7f592becedac325981
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-12 21:44:11 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,35 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-Combination 을 모두 출력하는 함수인데 중복 가능
-*/
+int N, M;
+string str;
 
-int n, m;
-int res[9];
-
-// 마지막으로 넣은 start를 사용하여 start 이후의 수만 넣는다.
-void permutation(int depth, int start) {
-    if (depth == m) {
-        for (int i = 0; i < m; i++){
-            cout << res[i] << ' ';
-        }
-        cout << '\n';
+void combination(int depth, int start){
+    if (depth == M){
+        cout << str << "\n";
         return;
     }
-
-    for (int i = start; i <= n; i++) {
-        res[depth] = i;
-        permutation(depth + 1, i);  // 다음 단계
+    for (int i = start; i <= N; i++){
+        str.push_back(i + '0');
+        str.push_back(' ');
+        combination(depth + 1, i);
+        str.pop_back();
+        str.pop_back();
     }
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n >> m;
-    permutation(0, 1);
-
-    return 0;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+    combination(0, 1);
 }
