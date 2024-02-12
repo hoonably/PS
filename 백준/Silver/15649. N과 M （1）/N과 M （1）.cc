507b1aa66984c13b1f435de3d7fedce984f00d70
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-12 21:39:18 +0900

    [Silver III] Title: N과 M (1), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,39 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-Permutation 을 모두 출력하는 함수
-순서도 중요하다.
-*/
+int N, M;
+string str;
+bool visited[9];
 
-int n, m;
-bool visited[9];  // 기본적으로 false로 초기화 된다.
-int res[9];
-
-void permutation(int depth) {
-    if (depth == m) {
-        for (int i = 0; i < m; i++){
-            cout << res[i] << ' ';
-        }
-        cout << '\n';
+void permutation(int depth){
+    if (depth == M){
+        cout << str << "\n";
         return;
     }
-
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= N; i++){
         if (visited[i]) continue;
         visited[i] = true;
-        res[depth] = i;
-        permutation(depth + 1);  // 다음 단계
-        visited[i] = false;  // 백트래킹
+        str.push_back(i + '0');
+        str.push_back(' ');
+        permutation(depth + 1);
+        visited[i] = false;
+        str.pop_back();
+        str.pop_back();
     }
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n >> m;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
     permutation(0);
-
-    return 0;
 }
