e9315a05a4d2b209e2b366e40aa30e2f4c4df526
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-12 21:32:12 +0900

    [Silver III] Title: N과 M (3), Time: 112 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,34 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
-/*
-nπr 중복순열이므로 visited를 체크할 필요가 없다.
-*/
 
-int n, m;
-int res[9];
+int N, M;
+string str;
 
-void permutation(int depth) {
-    if (depth == m) {
-        for (int i = 0; i < m; i++){
-            cout << res[i] << ' ';
-        }
-        cout << '\n';
+void go(int d){
+    if (d == M){
+        cout << str << "\n";
         return;
     }
-
-    for (int i = 1; i <= n; i++) {
-        res[depth] = i;
-        permutation(depth + 1);  // 다음 단계
+    for (int i = 1; i <= N; i++){
+        str.push_back(i + '0');
+        str.push_back(' ');
+        go(d + 1);
+        str.pop_back();
+        str.pop_back();
     }
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n >> m;
-    permutation(0);
-
-    return 0;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> M;
+    go(0);
 }
