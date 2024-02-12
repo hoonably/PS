0c1843aaa1f8146f99d9ff07a1b8936a50020299
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-12 18:40:14 +0900

    [Silver III] Title: 칸토어 집합, Time: 84 ms, Memory: 4256 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string kantore(int n) {
+    if (n == 0) {
+        return "-";
+    }
+    string str = kantore(n - 1);
+    for (int i=0; i<pow(3,n-1); i++){
+        str += ' ';
+    }
+    str += kantore(n - 1);
+    return str;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    while (!(cin >> N).eof()) {
+        cout << kantore(N) << endl;
+    }
+
+    return 0;
+}
