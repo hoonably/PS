8ba871767aa18ceb166d3f21f422a162122d6076
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-14 02:27:40 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -9,12 +9,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int T, N;
-    long dp[101];
-    dp[1] = 1;
-    dp[2] = 1;
-    dp[3] = 1;
-    dp[4] = 2;
-    dp[5] = 2;
+    long dp[101] {0, 1, 1, 1, 2, 2, };  // 이런식으로 앞에만 초기화 가능
+
     for (int i=6; i<=100; i++){
         dp[i] = dp[i-1] + dp[i-5];
     }
