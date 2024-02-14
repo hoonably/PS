ac9b3075af29339cad4a29f0c7df7301d2f7b92d
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-14 22:51:05 +0900

    [Gold V] Title: LCS, Time: 216 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold V/9251. LCS/LCS.py
@@ -0,0 +1,18 @@
+str1 = input()
+str2 = input()
+
+n = len(str1)
+m = len(str2)
+
+# dp[j] : 두번째 수열 j번째 인덱스까지 LCS
+dp=[0]*m
+
+for i in range(n):
+    cnt = 0
+    for j in range(m):
+        if cnt < dp[j]:
+            cnt = dp[j]
+        elif str1[i] == str2[j]:
+            dp[j] = cnt + 1
+            
+print(max(dp))
