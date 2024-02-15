2c54712df382f7cb01d6a3fa3ae4aa7802ddd9af
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-15 14:11:55 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver III/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+S[i] = 1~i의 합
+i ~ j 번째 수 합 : S[j] - S[i-1]
+*/
+
+int N, M, num, s, e;
+int S[100001];
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i=1; i<=N; i++){
+        cin >> num;
+        S[i] = S[i-1] + num;
+    }
+    while(M--){
+        cin >> s >> e;
+        cout << S[e]-S[s-1] << '\n';
+    }
+
+    
+    return 0;
+}
