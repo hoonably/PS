628dfef53e89781a090e81676199f48bc8217744
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-17 02:09:01 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -5,22 +5,22 @@ using namespace std;
 
 */
 
+long long N, cost, min_cost = 1e9, ans = 0;
+long long dist[100000];
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    int min_cost = 1e9, ans = 0;
-    int N, cost;
-
     cin >> N;
-    int div[N-1];
+    
     for (int i=0; i<N-1; i++){
-        cin >> div[i];
+        cin >> dist[i];
     }
     for (int i=0; i<N-1; i++){
         cin >> cost;
         min_cost = min(min_cost, cost);
-        ans += min_cost * div[i];
+        ans += min_cost * dist[i];
     }
     cin >> cost;
     cout << ans;
