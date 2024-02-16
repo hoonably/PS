fd6e9c65e40d2c07975c516c533e5e06a03c2253
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-16 21:41:29 +0900

    [Silver IV] Title: 로프, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+로프를 일단 정렬한 후
+버틸 수 있는 무게가 가장 큰거부터 걸고, 
+계속 순서대로 걸어나가면서 최대 중량을 올리고,
+
+최대중량 : 가장 약한 로프 * 총 로프 수
+        : rope[i] * i
+
+4
+
+3
+3
+3
+10
+
+입력시 3이 세개이므로 바로 break하면 틀림
+12가 정답임
+*/
+
+int N;
+double rope[100001];
+
+bool compare(int a, int b) {
+	return a > b;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> N;
+    for (int i=1; i<=N; i++){
+        cin >> rope[i];
+    }
+
+    sort(rope+1, rope+N+1, compare);
+
+    double sum = 0;
+    double best = 0;
+    for (int i=1; i<=N; i++){
+        // cout << rope[i] << ' ';
+        // // 최대가 갱신이 안된다면 앞으로 계속 갱신이 안됨
+        // if (rope[i] * i < best) break;
+
+        // // 더 좋아졌다면 best 대체
+        // else best = rope[i] * i;
+        best = max(best, rope[i]*i);
+    }
+    cout << (int)best;
+
+    return 0;
+}
