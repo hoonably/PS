042601fc53012d5809a41164ec2fa5d9b3e1e46d
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-18 22:23:49 +0900

    [Silver V] Title: 행렬 곱셈, Time: 4 ms, Memory: 2136 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+/*
+행렬의 곱셈이다.
+*/
+
+int A[100][100], B[100][100], C[100][100];
+
+int main() {
+    int N, M, K;
+    cin >> N >> M;
+
+    // Input matrix A
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cin >> A[i][j];
+        }
+    }
+    cin >> M >> K;
+
+    // Input matrix B
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < K; ++j) {
+            cin >> B[i][j];
+        }
+    }
+
+    // Matrix multiplication
+    for (int n = 0; n < N; ++n) {
+        for (int k = 0; k < K; ++k) {
+            for (int m = 0; m < M; ++m) {
+                C[n][k] += A[n][m] * B[m][k];
+            }
+        }
+    }
+
+    // Output
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < K; ++j) {
+            cout << C[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
