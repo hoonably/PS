eb2ff16232bd80a0dbbf7ec69db524b435491d2c
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-25 02:36:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -4,9 +4,7 @@ using namespace std;
 typedef long long ll;
 
 /*
-N : 정점의 개수
-M : 간선의 개수
-R : 시작 노드
+
 */
 
 int dy[] = {0, 0, 1, -1};
@@ -18,14 +16,13 @@ int graph[25][25];
 int bfs(int start_x, int start_y){
     queue<pair<int,int>> q;
     q.push({start_x,start_y});
-    graph[start_x][start_y]=1;
+    graph[start_x][start_y]=0;
     
     int cnt = 0; // 한단지 내의 집의 수
     
     while (!q.empty()){
         int x = q.front().first;
         int y = q.front().second;
-        graph[x][y]=0;
         cnt++;
         q.pop();
         for (int i=0; i<4; i++){
