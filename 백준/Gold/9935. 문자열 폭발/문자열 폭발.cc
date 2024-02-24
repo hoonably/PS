bc9a29aad25017374460db476c37ab050425267c
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-24 16:50:24 +0900

    [Gold IV] Title: 문자열 폭발, Time: 12 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -9,39 +9,35 @@ typedef long long ll;
 그리고 삭제했다면 다시 삭제한만큼 idx땡겨와서 계속 진행
 */
 
-string str;
-string bomb;
-int slen, blen, idx;
-char ans[1000000];
+string word, bomb, ans;
+int b_len;
 
-bool check(int start){
-    for (int i = start; i < start + blen; ++i) 
-        if (ans[i] != bomb[i - start]) 
-            return false;
+// 현재 ans의 뒤부분이 폭탄과 같은지 체크
+bool check(){
+    for (int i = 1; i <= b_len; i++) {
+        if (bomb[b_len - i] != ans[ans.length() - i]) return false;
+    }
     return true;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
-    cin >> str >> bomb;
-    slen = str.length();
-    blen = bomb.length();
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    idx = 0;
-    for (int i = 0; i < slen; i++) {
-        ans[idx++] = str[i];
-        if (idx >= blen && check(idx - blen))
-            idx -= blen;  // idx를 뒤로 밀기
+    cin >> word >> bomb;
+    b_len = bomb.length();
+
+    for (char c : word) {
+        ans += c;
+        // c가 폭탄 마지막 글자이면서 res의 길이가 폭탄의 크기 이상이면 체크
+        if (c == bomb[b_len - 1] && ans.size() >= bomb.size()) {
+
+            if (check()) ans.erase(ans.end() - b_len, ans.end());
+        }
     }
-    if (idx==0) {
-        cout << "FRULA";
-        return 0;
-    }
-    for (int i=0; i<idx; i++){
-        cout << ans[i];
-    }
-    
+
+    if (ans.empty()) cout << "FRULA";
+    else cout << ans;
+
     return 0;
 }
