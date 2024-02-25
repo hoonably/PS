6a18766a9c6f10ca975dc2136dd317acbc1ae149
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-25 18:24:19 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    if (N%2) cout << "SK";
+    else cout << "CY";
+    
+    return 0;
+}
