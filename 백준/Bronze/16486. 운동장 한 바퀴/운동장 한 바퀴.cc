07da9e0350d01a6f7fbbdc16afef35f3c66ce13a
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-26 01:57:45 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cout.precision(9);
+
+    int d1, d2;
+    cin >> d1 >> d2;
+    cout << 2*d1 + 2*M_PI*d2;
+    
+    return 0;
+}
