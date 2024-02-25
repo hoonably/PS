add08f4206e39cc6ad6632b9dcdd485b661f93d0
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-26 01:11:13 +0900

    [Bronze III] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T;
+    cin >> T;
+    while(T--){
+        int V, E;
+        cin >> V >> E;
+        cout << 2+E-V << '\n';
+    }
+    
+    return 0;
+}
