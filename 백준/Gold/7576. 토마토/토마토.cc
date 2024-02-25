40b4b1be2c9d8eba3f878d5f30627a7bbbd8f9d1
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-25 14:40:30 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 12488 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -19,8 +19,8 @@ int bfs(){
 
     // queue의 시작지점이 여러개
     queue<tuple<int,int,int>> q;
-    for (int i=0; i<M; i++){
-        for (int j=0; j<N; j++){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
             if (graph[i][j]==1){
                 q.push({i,j,0});
             }
@@ -35,7 +35,7 @@ int bfs(){
         for (int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if ( nx<0 || nx>=M || ny<0 || ny>=N ) continue;
+            if ( nx<0 || nx>=N || ny<0 || ny>=M ) continue;
             if (graph[nx][ny]==0) {
                 graph[nx][ny] = 1;
                 q.push({nx, ny, dist+1});
@@ -51,9 +51,9 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> N >> M;
-    for (int i=0; i<M; i++){
-        for (int j=0; j<N; j++){
+    cin >> M >> N;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
             cin >> graph[i][j];
         }
     }
@@ -61,8 +61,8 @@ int main(){
     int day = bfs();
     
     // 안익은(0)게 있다면
-    for (int i=0; i<M; i++){
-        for (int j=0; j<N; j++){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
             if (graph[i][j]==0){
                 cout << -1;
                 return 0;
