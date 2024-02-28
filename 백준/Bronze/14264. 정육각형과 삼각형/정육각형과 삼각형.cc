82b98e7a75e830eaa1347bf9dc3f079492dd292b
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-28 11:31:57 +0900

    [Bronze III] Title: 정육각형과 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    ll L;
+    cin >> L;
+    cout.precision(10);
+    cout << sqrt(3)/4*L*L;
+    
+    return 0;
+}
