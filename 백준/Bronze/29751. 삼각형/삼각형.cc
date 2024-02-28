adaef95f092b705dbf36a5b349f252564373b83b
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-28 11:36:56 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int W,H;
+    cin >> W >> H;
+    cout << fixed;
+    cout.precision(1);
+    cout << 0.5*W*H;
+    
+    return 0;
+}
