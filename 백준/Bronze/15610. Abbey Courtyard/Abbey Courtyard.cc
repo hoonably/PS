9dddd8e76085de929c9b80c75904daa4e75106d3
hoonably <77783081+hoonably@users.noreply.github.com>
2024-02-28 11:24:19 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    long a;
+    cin >> a;
+    double ra = sqrt(a);
+    cout.precision(9);
+    cout << 4*ra;
+    
+    return 0;
+}
