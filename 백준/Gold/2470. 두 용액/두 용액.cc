233503b492fb8f32f9e67b31bf8fdf398d8fef31
hoonably <77783081+hoonably@users.noreply.github.com>
2024-03-04 14:22:03 +0900

    [Gold V] Title: 두 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+/*
+
+*/
+
+int N;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N;
+    vector<int> arr(N);
+    for (int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+
+    int s = 0, e = N-1;
+    int best = 2e9+1;
+    int first;
+    int last;
+
+    while(s<e){
+        int sum = arr[s]+arr[e];
+
+        // 답 갱신
+        if (abs(sum) < best){
+            best = abs(sum);
+            first = arr[s];
+            last = arr[e];
+
+            if (best==0) break;
+        }
+
+
+        if (sum<0) s++;
+        else e--;
+    }
+
+    cout << first << ' ' << last;
+    
+    return 0;
+}
