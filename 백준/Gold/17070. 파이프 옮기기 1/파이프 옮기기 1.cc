845a449b573d445a7e43975a0c00c68a02372fd3
hoonably <77783081+hoonably@users.noreply.github.com>
2024-03-15 12:42:06 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MAX 17
+
+/*
+
+*/
+
+int N;
+bool graph[MAX][MAX];
+int dp[16][16][3];
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> N;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            cin >> graph[i][j];
+        }
+    }
+
+	dp[0][1][0] = 1;
+	for (int j = 2; j < N; j++)	// (0, 2)부터 (0, N-1)까지 체크
+		if (!graph[0][j])
+			dp[0][j][0] = dp[0][j - 1][0];
+
+	for (int i = 1; i < N; i++)	// 1행부터
+	{
+		for (int j = 2; j < N; j++)	// 2열부터
+		{
+			if (!graph[i][j])	// 빈칸이면
+			{
+				dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][1];
+				dp[i][j][2] = dp[i - 1][j][1] + dp[i - 1][j][2];
+				if (!graph[i-1][j] && !graph[i][j-1])
+					dp[i][j][1] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+			}
+		}
+	}
+
+
+    cout << dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+    
+    return 0;
+}
