0063f089000edd3cca502d51c58393e87d584abb
hoonably <77783081+hoonably@users.noreply.github.com>
2024-03-17 22:16:21 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 368 ms, Memory: 20976 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -4,10 +4,8 @@ typedef long long ll;
 #define MAX 100001
 
 /*
-1. 모든 간선들의 가중치를 오름차순으로 정렬
-2. 비용이 작은 간선 순으로 연결하려는 2개의 노드가 아직 서로 연결되지 않은 상태라면, 
-    2개의 노드를 서로 연결하는 과정 반복
-3. 간선이 (노드-1)개라면 MST이라 break해도 됨
+두 마을로 분리시키는거니
+최소 스패닝 트리 과정에서 가장 마지막에 이었던 비용을 빼줌
 */
 
 int V, E;
@@ -31,20 +29,21 @@ int MST(){
     }
 
     int ans = 0, cnt = 0;
-    int lastCost = 0;
     for (auto edge : v){
         auto [cost, now, next] = edge;
-        lastCost = cost;
 
         if (findParent(now) == findParent(next)) continue; // 이미 연결되어있다면 PASS
 
         unionParent(now, next);
         ans += cost;
 
-        if (++cnt == V-1) break;  // V-1개이면 바로 끝
+        if (++cnt == V-1) {
+            ans -= cost;
+            break;  // V-1개이면 바로 끝
+        }
     }
 
-    return ans - lastCost;
+    return ans;
 }
 
 int main(){
