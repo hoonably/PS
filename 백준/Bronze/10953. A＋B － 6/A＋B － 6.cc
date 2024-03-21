29b9a12ed83575655ea1f7378f140e5c72368ff8
hoonably <77783081+hoonably@users.noreply.github.com>
2024-03-21 11:49:22 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MAX 
+
+/*
+
+*/
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int T, A, B;
+    char com;
+
+    cin >> T;
+    while(T--){
+        cin >> A >> com >> B;
+        cout << A+B << '\n';
+    }
+
+    
+    return 0;
+}
