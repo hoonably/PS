a65c129d7ade18ca04e7e20103d05c8cefbcaa85
hoonably <77783081+hoonably@users.noreply.github.com>
2024-03-21 22:47:30 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -2,15 +2,14 @@
 using namespace std;
 
 /*
-힙은 항상 top만 뽑을 수 있으므로
-값의 -값을 넣어주고 출력할때도 -값으로 원상태로 출력한다.
+
 */
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
     int N;
     cin >> N;
     for (int i=0; i<N; i++){
@@ -19,12 +18,12 @@ int main(){
         if (getNum==0){
             if (pq.empty()) cout << 0 << '\n';
             else {
-                cout << -pq.top() << '\n';
+                cout << pq.top() << '\n';
                 pq.pop();
             }
         }
         else{
-            pq.push(-getNum);
+            pq.emplace(getNum);
         }
     }
     
