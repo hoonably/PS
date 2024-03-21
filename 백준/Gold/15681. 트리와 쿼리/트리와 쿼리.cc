133006360f761c73abaf5f397416557d73166d4e
hoonably <77783081+hoonably@users.noreply.github.com>
2024-03-21 11:11:25 +0900

    [Gold V] Title: 트리와 쿼리, Time: 56 ms, Memory: 12580 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -6,7 +6,7 @@ typedef pair<int,int> pii;
 
 
 /*
-
+서브트리의 개수를 만들면서 트리를 만드는 dfs
 */
 
 vector<int> v[MAX];
@@ -15,16 +15,19 @@ int num[MAX];
 int node, query, s, e;
 
 
-int dfs(int n) {
-	if (num[n] != 0) return num[n];
-	visited[n] = true;
-	int ret = 1;
-	for (int i = 0; i < v[n].size(); i++) {
-		int next = v[n][i];
+int dfs(int now) {
+
+	if (num[now] != 0) return num[now];
+	visited[now] = true;
+
+	int ret = 1; // 자신도 자신을 루트로 하는 서브트리에 포함되므로 0이 아닌 1에서 시작한다.
+	for (int next : v[now]) {
 		if (visited[next]) continue;
 		ret += dfs(next);
 	}
-	num[n] = ret;
+
+	num[now] = ret;
+
 	return ret;
 }
 
@@ -33,15 +36,20 @@ int main(){
 
 	int root;
 	cin >> node >> root >> query;
+
 	for (int i = 0; i < node - 1; i++) {
 		cin >> s >> e;
 		v[s].push_back(e);
 		v[e].push_back(s);
 	}
+    
+	// root 를 기준으로 트리를 생성하면서 그 서브트리의 개수 저장
 	num[root] = dfs(root);
+
 	for (int i = 0; i < query; i++) {
 		cin >> s;
 		cout << num[s] << '\n';
 	}
+
 	return 0;
 }
