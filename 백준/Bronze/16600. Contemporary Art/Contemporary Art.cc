e19f0dca8e420dd405e7901684be6ef31a6fc5b1
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-05 11:16:55 +0900

    [Bronze IV] Title: Contemporary Art, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define MAX
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    cout << fixed;
+    cout.precision(10);
+    ll a;
+    cin >> a;
+    cout << 4 * sqrt(a);
+
+
+    return 0;
+}
