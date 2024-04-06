7efc9a819ad3c3281fc49b7e04155ca71789e3ed
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-06 21:00:48 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define MAX
+
+/*
+
+*/
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int N;
+    cin >> N;
+    if (N%2==1) cout << "evoLmoCicS";
+    else cout << "SciComLove";
+
+    return 0;
+}
