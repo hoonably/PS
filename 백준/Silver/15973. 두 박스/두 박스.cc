87ef4abaa630e3340ae4f2a6f11c1d724b63e894
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-06 13:26:15 +0900

    [Silver I] Title: 두 박스, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15973. 두 박스/두 박스.cc
+++ 백준/Silver/15973. 두 박스/두 박스.cc
@@ -4,7 +4,6 @@ typedef long long ll;
 typedef pair<int,int> pii;
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define MAX
-#define y1 notUsed
 
 /*
 
@@ -12,27 +11,46 @@ typedef pair<int,int> pii;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    
-    int x1, y1, x2, y2, _x1, _y1, _x2, _y2;
-    cin >> x1 >> y1 >> x2 >> y2;
-    cin >> _x1 >> _y1 >> _x2 >> _y2;
-    if(x1 > _x1){
-        swap(x1, _x1);
-        swap(y1, _y1);
-        swap(x2, _x2);
-        swap(y2, _y2);
+
+    int l_x1, l_y1, r_x1, r_y1;
+    int l_x2, l_y2, r_x2, r_y2;
+    cin >> l_x1 >> l_y1 >> r_x1 >> r_y1;
+    cin >> l_x2 >> l_y2 >> r_x2 >> r_y2;
+
+    // 좌측하단 꼭지점이 더 좌측에 있는 사각형이 1번
+    if(l_x1 > l_x2) {
+        swap(l_x1, l_x2);
+        swap(l_y1, l_y2);
+        swap(r_x1, r_x2);
+        swap(r_y1, r_y2);
     }
-    if(x2 == _x1){
-        if(y2 == _y1 || y1 == _y2){
-            cout << "POINT" << "\n";
-        }else cout <<"LINE" << "\n";
-        //겹치거나. 
-    }else if(x2 > _x1){
-        //아예 박으로 나가거나. 
-        if(y2 < _y1 || y1 > _y2) cout << "NULL" << "\n"; 
-        else if(y2 == _y1 || y1 == _y2) cout << "LINE" << "\n"; 
-        else cout << "FACE" << "\n";
-    }else if(x2 < _x1) cout << "NULL" << "\n"; 
+
+    // 왼쪽 사각형의 우측면과, 오른쪽 사각형의 좌측면이 같은 x값일때
+    if(r_x1 == l_x2) {
+        // 왼쪽 사각형 우측상단 점과, 오른쪽 사각형 좌측하단 점이 같을때 or
+        // 왼쪽 사각형 우측하단 점과, 오른짝 사각형 좌측상단 점이 같을때 POINT
+        if(r_y1 == l_y2 || l_y1 == r_y2) cout << "POINT";
+            // 왼쪽 사각형의 윗면이, 오른쪽 사각형의 아랫면보다 아래 있을때
+        else if(l_y2 > r_y1) cout << "NULL";
+            // 왼쪽 사각형의 아랫면이, 오른쪽 사각형의 윗면보다 위에 있을때
+        else if(l_y1 > r_y2) cout << "NULL";
+        else cout << "LINE";
+    }
+        // 오른쪽 사각형의 왼쪽면이, 왼쪽 사각형의 오른쪽 면보다 왼쪽에 있을 경우
+    else if(r_x1 > l_x2) {
+        // 왼쪽 사각형의 윗면과, 오른쪽 사각형의 아랫면이 같을떄
+        if(r_y1 == l_y2) cout << "LINE";
+            // 왼쪽 사각형의 아랫면과, 오른쪽 사각형의 윗면이 같을때
+        else if(l_y1 == r_y2) cout << "LINE";
+            // 왼쪽 사각형의 윗면이, 오른쪽 사각형의 아랫면보다 아래 있을때
+        else if(r_y1 < l_y2) cout << "NULL";
+            // 왼쪽 사각형의 아랫면이, 오른쪽 사각형의 윗면보다 위에 있을때
+        else if(l_y1 > r_y2) cout << "NULL";
+        else cout << "FACE";
+    }
+        // 왼쪽 사각형의 오른쪽면이, 오른쪽 사각형의 왼쪽면보다 왼쪽에 있을때
+    else
+        cout << "NULL";
 
 
     return 0;
