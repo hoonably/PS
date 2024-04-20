7a8200bbf25dea9375c98b7ae90ad4a8dc9ab565
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-21 03:08:29 +0900

    [Platinum IV] Title: 도미노, Time: 132 ms, Memory: 28060 KB -BaekjoonHub

--- 백준/Platinum/4196. 도미노/도미노.cc
+++ 백준/Platinum/4196. 도미노/도미노.cc
@@ -10,7 +10,8 @@ SCC를 구하는 타잔 알고리즘(Tarjan's Algorithm) 이지만,
 => indegree가 0인 SCC의 개수가 답
 */
 
-int id, d[MAX];
+int id;  // 부모값을 초기화 하는 변수
+int d[MAX];  // SCC 부모값을 저장하는 배열
 bool finished[MAX];
 int group[MAX];  // 각 도미도(노드)가 몇 번째 SCC에 속하는지 그룹으로 기록하는 배열
 int indegree[MAX];  // 각 SCC를 하나의 정점으로 간주하고 정점의 진입 차수를 기록하는 배열
@@ -26,7 +27,7 @@ int dfs(int x){
     int parent = d[x];  // 자신이 부모
     for (int child : v[x]){
         // 방문 안한 노드
-        if (d[child] == 0) parent = min(parent, dfs(child));
+        if (d[child] == -1) parent = min(parent, dfs(child));
         // 방문은 했지만 처리가 안된 노드
         else if (!finished[child]) parent = min(parent, d[child]);
     }
@@ -38,7 +39,7 @@ int dfs(int x){
             int t = s.top();
             s.pop();
             scc.push_back(t);
-            group[t] = SCC.size() + 1;
+            group[t] = SCC.size() + 1;  // 그룹 번호 저장
             finished[t] = true;
             if (t==x) break;
         }
@@ -48,20 +49,20 @@ int dfs(int x){
     return parent;  // 자신의 부모 값을 반환
 }
 
+void init(int N){
+    id = 0;
+    memset(d, -1, sizeof(d));
+    memset(finished, false, sizeof(finished));
+    memset(indegree, 0, sizeof(indegree));
+    for (int i=0; i<=N; i++) v[i].clear();
+    SCC.clear();
+}
+
 void solve(){
     int N, M;
     cin >> N >> M;
 
-    // 초기화
-    id = 0;
-    SCC.clear();
-    for (int i=0; i<=N; i++){
-        d[i] = 0;
-        finished[i] = false;
-        v[i].clear();
-        indegree[i]=0;
-        group[i]=0;
-    }
+    init(N);
 
     for (int i=0; i<M; i++){
         int s, e;
@@ -70,16 +71,21 @@ void solve(){
     }
 
     for (int i=1; i<=N; i++){
-        if (d[i]==0) dfs(i);
-    }
+        if (d[i]==-1) dfs(i);
+    }  // 여기까지 SCC 완료
 
+    
     for (int i=1; i<=N; i++){
-        for (int w : v[i]){
-            if (group[w]==group[i]) continue;
-            indegree[group[w]]++;
+        for (int w : v[i]){  // i번 도미노에 연결된 도미노들 중 
+            // 서로 다른 그룹이라면 그 그룹의 진입차수 증가시킴
+            // 왜냐면 i를 무너뜨리면 w가 속해있는 그룹도 무너지는것이므로
+            if (group[w]!=group[i]){ 
+                indegree[group[w]]++;  
+            }
         }
     }
     
+    // 이제 indegree(진입차수)가 0인 곳의 개수를 출력
     int ans = 0;
     for (int i=0; i<SCC.size(); i++){
         if (indegree[i]==0) ans++;
