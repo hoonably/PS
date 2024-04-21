b33caee5e1642249ed51949df03603cb0c6d8091
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-21 22:53:07 +0900

    [Platinum IV] Title: 축구 전술, Time: 160 ms, Memory: 9104 KB -BaekjoonHub

--- 백준/Platinum/3977. 축구 전술/축구 전술.cc
+++ 백준/Platinum/3977. 축구 전술/축구 전술.cc
@@ -18,15 +18,17 @@ SCC가 indegree(들어오는 간선)을 가지고 있다면 시작 지점이 될
 => indegree가 0인 SCC가 2개면 서로 절대 못가기 때문에 불가능함 -> Confused
 
 => indegree가 0인 SCC가 1개일 경우 답을 찾아내야함
+
+이 문제에서는 SCC를 굳이 2차원 벡터로 구할 필요가 없다. sccSize와 어떤 SCC에 속하는지만 알면 된다.
 */
 
+int sccSize;
 int id;  // 부모값을 초기화 하는 변수
 int d[MAX];  // SCC 부모값을 저장하는 배열
 bool finished[MAX];
 int SCCID[MAX];  // 각 노드가 몇 번째 SCC에 속하는지
 int indegree[MAX];  // 각 SCC의 진입 차수 (여기서 인덱스는 SCC의 번호)
 vector<int> v[MAX];
-vector<vector<int>> SCC;
 stack<int> s;
 
 // DFS는 총 정점의 갯수만큼 식행 된다.
@@ -44,16 +46,14 @@ int dfs(int x){
 
     // 부모 노드가 자신인 경우 (사이클인 경우)
     if (parent == d[x]){
-        vector<int> scc;
         while(true){
             int t = s.top();
             s.pop();
-            scc.push_back(t);
-            SCCID[t] = SCC.size();  // 그룹 번호 저장 (0번부터)
+            SCCID[t] = sccSize;  // 그룹 번호 저장 (0번부터)
             finished[t] = true;
             if (t==x) break;
         }
-        SCC.push_back(scc);  // 그룹 추가
+        sccSize++;  // SCC가 추가됨
     }
 
     return parent;  // 자신의 부모 값을 반환
@@ -61,12 +61,12 @@ int dfs(int x){
 
 void init(int N){
     id = 0;
+    sccSize = 0;
     memset(d, -1, sizeof(int)*N);
     memset(finished, false, sizeof(bool)*N);
     memset(SCCID, -1, sizeof(int)*N);
     memset(indegree, 0, sizeof(int)*N);
     for (int i=0; i<N; i++) v[i].clear();
-    SCC.clear();
 }
 
 void solve(){
@@ -85,11 +85,9 @@ void solve(){
         if (d[i]==-1) dfs(i);
     }  // 여기까지 SCC 완료
 
-    
+    // 진입차수
     for (int i=0; i<N; i++){
-        for (int nxt : v[i]){  // i번 도미노에 연결된 도미노들 중 
-            // 서로 다른 그룹이라면 그 그룹의 진입차수 증가시킴
-            // 왜냐면 i를 무너뜨리면 w가 속해있는 그룹도 무너지는것이므로
+        for (int nxt : v[i]){
             if (SCCID[nxt]!=SCCID[i]){ 
                 indegree[SCCID[nxt]]++;  
             }
@@ -105,7 +103,7 @@ void solve(){
     // return;
 
     int notInIdx = -1;  // 진입 불가능한 그룹의 번호
-    for (int i=0; i<SCC.size(); i++){
+    for (int i=0; i<sccSize; i++){
         if (indegree[i]==0) {
             if (notInIdx==-1) {
                 notInIdx = i;
