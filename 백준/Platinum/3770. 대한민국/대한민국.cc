ddd79a774bbec4db4487d4cfdc452a68e43ab173
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-24 14:19:26 +0900

    [Platinum V] Title: 대한민국, Time: 112 ms, Memory: 5144 KB -BaekjoonHub

--- 백준/Platinum/3770. 대한민국/대한민국.cc
+++ 백준/Platinum/3770. 대한민국/대한민국.cc
@@ -5,8 +5,15 @@ typedef pair<int,int> pii;
 #define MAX 1'001
 
 /*
-세그먼트 트리로 버블소트할때 swap이 몇번 발생할지
-swap의 개수 : 자신보다 작은 수의 개수
+펜윅 트리 사용
+펜윅트리는 세그먼트트리를 응용한 자료구조라고 볼 수 있다.
+1번 부터 현재 Index까지의 연산 결과를 저장하는 '누적'개념을 사용
+
+메모리가 훨씬 더 절약되고, 코드가 간결하고 짧다는 장점
+
+구간에 대한 연산결과가 아닌, 특정 값만을 찾기를 원한다면 
+펜윅트리는 구현이 힘들다.
+ex) 최댓값 or 최솟값 찾기
 
 */
 
@@ -19,23 +26,30 @@ struct FenwickTree {
     // 생성자
     FenwickTree(int n) : tree(n+1) {};
 
-    int sum(int pos) {
+    // 1 ~ idx번 누적합 구하기
+    int sum(int idx) {
         int ret = 0;
-        while (pos > 0) {
-            ret += tree[pos];
-            pos &= (pos - 1);
+        while (idx > 0) {
+            ret += tree[idx];
+            idx &= (idx - 1);
         }
         return ret;
     }
-
-    void add(int pos, int val) {
-        while (pos < tree.size()) {
-            tree[pos] += val;
-            pos += (pos & -pos);  
-            // pos & -pos : 가장 오른쪽에 있는 비트 1
+    
+    void update(int idx, int val) {
+        while (idx < tree.size()) {
+            tree[idx] += val;
+            idx += (idx & -idx); 
+            // idx & -idx : 1이 존재하는 '최하위' 있는 비트
             // pos가 1001100 이라면 100 반환
             // 이진 인덱스 트리나 펜윅 트리에서 
             // 특정 위치 이후에 오는 다음 위치를 찾는 데 사용
+        }
+    }
+
+    void makeTree(int arr[]){
+        for (int i=1; i<=N; i++){
+            update(i, arr[i]);
         }
     }
 };
@@ -56,7 +70,7 @@ ll solve(){
         if (edges[i].second != M) {
             ans += fwTree.sum(M - edges[i].second);
         }
-        fwTree.add(M + 1 - edges[i].second, 1);
+        fwTree.update(M + 1 - edges[i].second, 1);
     }
     return ans;
 }
