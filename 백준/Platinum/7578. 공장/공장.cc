8318c10947fb5b2f863b7723aa85facc30518c7d
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-24 13:24:22 +0900

    [Platinum V] Title: 공장, Time: 324 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.cc
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -14,32 +14,35 @@ pair을 사용하여 second에 인덱스를 찾는다.
 int N;
 int id_to_idx[1'000'001];  // 기계의 식별번호 0 이상 1,000,000 이하
 int arr[MAX];
-vector<int> segTree;
+vector<ll> segTree;
 
-int query(int idx, int s, int e, int target) {
-	if (e < target) return 0;
-	if (target <= s) return segTree[idx];
-	if (s == e) return segTree[idx];
-
-	int lsum = query(idx * 2, s, (s + e) / 2, target);
-	int rsum = query(idx * 2 + 1, (s + e) / 2 + 1, e, target);
-	return lsum + rsum;
+// i > j를 만족하는 것의 개수만 카운트
+ll sum(int node, int start, int end, int left, int right){
+    if (left > end || right < start)
+        return 0;
+    if (left <= start && right >= end)
+        return segTree[node];
+    int mid = (start + end) / 2;
+    return sum(node*2, start, mid, left, right) 
+    + sum(node*2 + 1, mid + 1, end, left, right);
 }
 
-void update(int idx, int s, int e, int target) {
-	if (target < s || e < target) return;
-	if (s == e) {
-		segTree[idx] = 1;
-		return;
-	}
-	update(idx * 2, s, (s + e) / 2, target);
-	update(idx * 2 + 1, (s + e) / 2 + 1, e, target);
-	segTree[idx]++;
+// 카운트 후 업데이트
+void update(int node, int start, int end, int idx){
+    if (start==end){
+        segTree[node] = 1;
+        return;
+    }
+    int mid = (start + end) / 2;
+    if (idx <= mid) update(2 * node, start, mid, idx);
+    else update(2 * node + 1, mid + 1, end, idx);
+    segTree[node] = segTree[2*node] + segTree[2*node+1];
 }
 
-int main() {
-	int N;
-	cin >> N;
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> N;
 
     // 트리의 크기
     int h = ceil(log2(N));  // ceil : 정수로 올림
@@ -56,11 +59,15 @@ int main() {
 		arr[i] = id_to_idx[id];
 	}
 
-	long long ans = 0;
-	for (int i = 0; i < N; i++) {
-		ans += query(1, 0, N - 1, arr[i]);
-		update(1, 0, N - 1, arr[i]);
-	}
-
-	cout << ans;
+    // i > j이면서 arr[i] < arr[j]인 쌍의 개수
+    ll ans = 0;
+    for (int i=0; i<N; i++){
+        int idx = arr[i];
+        // idx보다 큰 수의 개수 찾기
+        ans += sum(1, 0, N-1, idx+1, N-1);
+        update(1, 0, N-1, idx);
+    }
+    cout << ans;
+    
+    return 0;
 }
