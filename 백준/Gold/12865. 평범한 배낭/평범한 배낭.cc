9edd93f7965de76b08aec77ca0ed5b2e6370aa0a
hoonably <77783081+hoonably@users.noreply.github.com>
2024-04-30 00:02:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define all(v) v.begin(), v.end()
+#define MAX
 
 /*
 냅색 알고리즘
@@ -10,8 +15,7 @@ int N, K;
 int W[101], V[101], dp[100001];
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
     
     cin >> N >> K;
 
@@ -20,9 +24,10 @@ int main(){
     }
 
     for (int i=0; i<N; i++){
+        int weight = W[i];
         // 이거 탑다운으로 안하면 같은 물건을 여러번 넣는 경우도 계산됨
         for (int j=K; j>=W[i]; j--){
-            dp[j] = max(dp[j], dp[j-W[i]] + V[i]);
+            dp[j] = max(dp[j], dp[j-weight] + V[i]);
         }
     }
 
