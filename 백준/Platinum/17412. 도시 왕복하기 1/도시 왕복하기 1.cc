177fe00d37cd83c49f8bd4f8dbc963b87296861a
hoonably <77783081+hoonably@users.noreply.github.com>
2024-05-23 11:49:54 +0900

    [Platinum IV] Title: 도시 왕복하기 1, Time: 4 ms, Memory: 3484 KB -BaekjoonHub

--- 백준/Platinum/17412. 도시 왕복하기 1/도시 왕복하기 1.cc
+++ 백준/Platinum/17412. 도시 왕복하기 1/도시 왕복하기 1.cc
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define FOR(now,a,b) for(int now=(a);now<(b);now++)
 #define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
@@ -15,68 +15,109 @@ const int MOD = 1'000'000'007;
 /* -----------------------------------------------------
 https://www.acmicpc.net/problem/17412
 네트워크 플로우
+에드몬드 카프 알고리즘
+시간 복잡도 : O(VE^2)
+
 
 */
 
 #define MAX 401
 
-int N, P;
-int flow[MAX][MAX];
-int cap[MAX][MAX];
-vector<vector<int>> grid(MAX);
+class FordFulkerson{
+	private:
+		vector<vector<int>> flow, capacity, adjList;
+		int s, e, N; //source, sink, verties
+	public:
+		FordFulkerson(){ N = 0, s = -1, e = -1; }
 
-int edmonds_karp(int start, int end) {
-    int ret = 0;
-    while (true) {
-        queue<int> q;
-        q.push(start);  // 출발
-        vector<int> parent(N + 1, -1);
-        int minflow = INF;
+		FordFulkerson(int N){
+			this->N = N;
+			flow.resize(N+1), capacity.resize(N+1), adjList.resize(N+1);
+			for(int i=0; i<=N; i++){
+				flow[i].resize(N+1);
+				capacity[i].resize(N+1);
+			}
+			s = -1, e = -1;
+		}
 
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (int next : grid[cur]) {
-                if (cap[cur][next] - flow[cur][next] > 0 && parent[next] == -1) {
-                    q.push(next);
-                    parent[next] = cur;
-                    if (next == end) break;
-                }
-            }
-        }
+        // source와 sink가 주어진 경우
+		FordFulkerson(int N, int source, int sink){
+			this->N = N;
+			flow.resize(N+1), capacity.resize(N+1), adjList.resize(N+1);
+			for(int i=0; i<=N; i++){
+				flow[i].resize(N+1);
+				capacity[i].resize(N+1);
+			}
+			s = source, e = sink;
+		}
 
-        // 도착지점
-        if (parent[end] == -1) break;  // 도달 실패
+		void setSource(int t){ s = t; }
+		void setSink(int t){ e = t; }
 
-        // 거꾸로 최소 유량 찾기
-        for (int i = end; i != start; i = parent[i]) {
-            minflow = min(minflow, cap[parent[i]][i] - flow[parent[i]][i]);
-        }
+        // edge 만들기
+		void addEdge(int start, int end, int cap, bool directed){
+			adjList[start].push_back(end);
+			adjList[end].push_back(start);
+			capacity[start][end] += cap;
+			if(!directed) capacity[end][start] += cap;  // 무방향 그래프
+		}
 
-        // 최소 유량만큼 추가
-        for (int i = end; i != start; i = parent[i]) {
-            flow[parent[i]][i] += minflow;
-            flow[i][parent[i]] -= minflow;
-        }
-        ret += minflow;
-    }
+		int maxFlow(){
+            int ret = 0;
+			// if(s==-1 || e==-1) return -1;  // 오류 방지
+			vector<int> par(N+1);
 
-    return ret;
-}
+			while(true){
+				fill(par.begin(), par.end(), -1);
+
+				queue<int> q;
+                q.push(s);
+				while(!q.empty()){
+					int now = q.front(); q.pop();
+					for(auto nxt : adjList[now]){
+						if(par[nxt] == -1 && capacity[now][nxt]-flow[now][nxt] > 0){
+							q.push(nxt); par[nxt] = now;
+							if(nxt == e) break;
+						}
+					}
+				}
+
+                // 종료
+				if(par[e] == -1) break;
+
+                // 거꾸로 최소 유량 탐색
+				int minFlow = INF;
+				for(int i=e; i!=s; i = par[i]){
+					int a = par[i], b = i;
+					minFlow = min(minFlow, capacity[a][b] - flow[a][b]);
+				}
+				ret += minFlow;
+
+                // 최소 유량 처리
+				for(int i=e; i!=s; i = par[i]){
+					flow[par[i]][i] += minFlow; 
+                    flow[i][par[i]] -= minFlow;
+				}
+			}
+			return ret;
+		}
+};
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    
+	
+	int N, P;
     cin >> N >> P;
+
+	FordFulkerson ff(N, 1, 2);
+
     for(int i=0; i<P; i++){
         int a, b;
         cin >> a >> b;
-        grid[a].push_back(b);
-        grid[b].push_back(a);
-        cap[a][b] = 1;
+		ff.addEdge(a, b, 1, 1);
     }
 
-    cout << edmonds_karp(1, 2);
+    cout << ff.maxFlow();
     
     return 0;
 }
