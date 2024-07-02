5af5729865932572ac4f4f88a9349444f9af3336
hoonably <77783081+hoonably@users.noreply.github.com>
2024-07-02 12:01:50 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+const int dx[6] = { 1,-1,0,0,0,0 };
+const int dy[6] = { 0,0,1,-1,0,0 };
+const int dz[6] = { 0,0,0,0,1,-1 };
+const int INF = 0x7f7f7f7f;
+const ll LINF = 0x7f7f7f7f7f7f7f7f;
+const int MOD = 1'000'000'007;
+
+/* -----------------------------------------------------
+
+*/
+
+#define MAX 
+
+string str;
+int ans;
+
+// 팰린드롬인가?
+bool isPalindrom(string str) {
+    int len = str.length()/2;
+    for(int i=0; i<len; i++){
+        if(str[i]!=str[str.length()-1-i]){
+            return false;
+        }
+    }
+ 
+    return true;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> str;
+    ans = str.length()*2-1;  // 최악
+
+    // 가운데 부분 문자열 중에서 팰린드롬인게 있는가?
+    for(int i=0; i<str.length(); i++){
+
+        // i번째 인덱스~길이-i번째 인덱스
+        string temp = str.substr(i, str.length()-i);
+
+        // i개를 더 붙이면 팰린드롬이라는 거임
+        if(isPalindrom(temp)){
+            ans = str.length()+i;
+            break;
+        }
+    }
+ 
+    cout << ans << "\n";
+    
+    return 0;
+}
