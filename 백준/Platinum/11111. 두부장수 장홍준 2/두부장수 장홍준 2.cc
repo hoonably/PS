29d2e38dda408f314adc506ddf5fda52ded4a0c2
hoonably <77783081+hoonably@users.noreply.github.com>
2024-07-24 01:10:03 +0900

    [Platinum II] Title: 두부장수 장홍준 2, Time: 8 ms, Memory: 2668 KB -BaekjoonHub

--- 백준/Platinum/11111. 두부장수 장홍준 2/두부장수 장홍준 2.cc
+++ 백준/Platinum/11111. 두부장수 장홍준 2/두부장수 장홍준 2.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#define FOR(i,workNum,cost) for(int i=(workNum);i<(cost);i++)
-#define all(to) to.begin(), to.end()
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> pii; typedef pair<ll,ll> pll;
@@ -25,8 +25,7 @@ SPFA : 바뀐 정점은 큐를 이용해서 관리하고, 큐에 해당 정점�
 
 const int SZ = 2510, SRC = 2501, SINK = 2502;
 
-struct MCMF{
-	int SRC, SINK; //source, sink
+struct MCMF{  // use Dinic
 	struct Edge{ int to, cap, cost, rev; };
 	vector<Edge> graph[SZ];
 	void addEdge(int _from, int _to, int _cap, int _cost){
@@ -39,55 +38,38 @@ struct MCMF{
 			graph[i].clear();
 	}
 
-	int h[SZ], inQ[SZ];
+	bool inQ[SZ];
 	int dists[SZ]; //dijkstra
-	void init(int _s, int _t){
-		// SPFA : Shortest Path Faster Algorithm
-		// Bellman-Ford로 업데이트 : O(VE)
-		// johnson'SRC, SPFA 사용시 O(V+E)
-		SRC = _s, SINK = _t;
-        memset(h, INF, sizeof(h));
-        memset(dists, INF, sizeof(dists));
-
-		//johnson'SRC algorithm with spfa
-        queue<int> q; q.push(SRC); inQ[SRC] = 1;
-        while(!q.empty()){
-            int now = q.front(); q.pop(); inQ[now] = 0;
-            for(auto next : graph[now]){
-                if(next.cap && h[next.to] > h[now] + next.cost){
-                    h[next.to] = h[now] + next.cost;
-                    if(!inQ[next.to]) inQ[next.to] = 1, q.push(next.to);
-                }
-            }
-        }
-        for(int i=0; i<SZ; i++){
-            for(auto &j : graph[i]) if(j.cap) j.cost += h[i] - h[j.to];
-        }
-
-		//get shortest path DAG with dijkstra
-        priority_queue<pii> pq; pq.emplace(0, SRC); dists[SRC] = 0;
-        while(pq.size()){
-            int now = pq.top().second;
-            int retCost = -pq.top().first;
-            pq.pop();
-            if(dists[now] - retCost) continue;
-            for(auto i : graph[now]){
-                if(i.cap && dists[i.to] > dists[now] + i.cost){
+    bool spfa() {
+        memset(dists, 0x3f, sizeof(dists));  // = 0x3f3f3f3f = 106,119,567
+        memset(inQ, false, sizeof(inQ));
+        queue<int> q;
+        q.push(SRC);
+        inQ[SRC] = true;
+        dists[SRC] = 0;
+        while (q.size()) {
+            int now = q.front();
+            q.pop();
+            inQ[now] = false;
+            for (auto i: graph[now]) {
+                if (i.cap && dists[i.to] > dists[now] + i.cost) {
                     dists[i.to] = dists[now] + i.cost;
-                    pq.emplace(-dists[i.to], i.to);
+                    if (!inQ[i.to]) inQ[i.to] = true, q.push(i.to);
                 }
             }
         }
-        for(int i=0; i<SZ; i++) dists[i] += h[SINK] - h[SRC];
-	}
+        return dists[SINK] < 1e9;
+    }
 
-	int chk[SZ], work[SZ];
+	bool chk[SZ];
+	int work[SZ];
 	bool update(){
 		int minflow = 1e9;
         for(int i=0; i<SZ; i++){
             if(!chk[i]) continue;
             for(auto j : graph[i]){
-                if(j.cap && !chk[j.to]) minflow = min(minflow, dists[i] + j.cost - dists[j.to]);
+                if(j.cap && !chk[j.to])
+					minflow = min(minflow, dists[i] + j.cost - dists[j.to]);
             }
         }
 		if(minflow >= 1e9) return 0;
@@ -98,7 +80,7 @@ struct MCMF{
 	}
 
 	int dfs(int now, int flow){
-        chk[now] = 1;
+        chk[now] = true;
         if(now == SINK) return flow;
         for(; work[now] < (int)graph[now].size(); work[now]++){
             auto &i = graph[now][work[now]];
@@ -112,24 +94,24 @@ struct MCMF{
         }
         return 0;
 	}
-	pii run(int _s, int _t){ //{최소비용, 최대유량} 반환
-		init(_s, _t);
-		int retCost = 0, retFlow = 0;
-		do{
-			memset(chk, 0, sizeof chk);
+
+    pair<int, int> run() {
+        int cost = 0, flow = 0;
+        while (spfa()) {
+            memset(chk, 0, sizeof chk);
             memset(work, 0, sizeof work);
-			int now = 0;
-			while(true){
+            int now = 0;
+            while (true) {
 				now = dfs(SRC, 1e9);
 				if (now==0) break;
-				retCost += dists[SINK] * now;
-				retFlow += now;
-				memset(chk, 0, sizeof chk);
-			}
-		}while(update());
-		return {retCost, retFlow};
-	}
-}mcmf;
+                cost += dists[SINK] * now;
+                flow += now;
+                memset(chk, 0, sizeof chk);
+            }
+        }
+        return {cost, flow};
+    }
+} mcmf;
 
 int prices[6][6] = {
 //   A   B  C  D     F
@@ -162,7 +144,7 @@ int main(){
         for (int c=0; c<M; c++){
             nowOrder++;
             
-            // 짝수칸은 SINK와만 연결
+            // 홀수번째칸은 SINK와만 연결
             if ((r + c) % 2) {
                 mcmf.addEdge(nowOrder, SINK, 1, 0);
                 continue;
@@ -189,6 +171,5 @@ int main(){
         }
     }
 
-	pii ans = mcmf.run(SRC, SINK);
-	cout << -ans.first;  // 최대비용
+	cout << -mcmf.run().first;  // 최대비용
 }
