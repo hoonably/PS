1e829fc439e6bf6a5539d192032fa69c1f128596
hoonably <77783081+hoonably@users.noreply.github.com>
2024-07-25 04:01:45 +0900

    [Platinum II] Title: 격자 0 만들기, Time: 48 ms, Memory: 2528 KB -BaekjoonHub

--- 백준/Platinum/11495. 격자 0 만들기/격자 0 만들기.cc
+++ 백준/Platinum/11495. 격자 0 만들기/격자 0 만들기.cc
@@ -17,92 +17,73 @@ https://www.acmicpc.net/problem/11495
 
 https://everenew.tistory.com/182
 
+간선의 양이 많아
+디닉 알고리즘 사용시 56ms
+포드 풀커슨 사용시 5600ms
 */
 
 #define MAX 
 
-struct MaximumFlow
-{
-	struct Edge
-	{
-		int from, to, cap, rev;
-		Edge(int from, int to, int cap) : from(from), to(to), cap(cap) {};
-	};
-	vector<vector<Edge>> graph;
-	vector<int> work, level;
-	int N, SRC, SINK;  // SOURCE(시작) => node => SINK(끝)
+const int SZ = 2510, SRC = 2501, SINK = 2502;
 
-	// 생성자 (SRC, BRIDGE, SINK 위해서 10개 여분)
-	MaximumFlow(int N, int SRC, int SINK) : N(N), SRC(SRC), SINK(SINK) {
-		graph.resize(N+10);
-		work.resize(N+10);
-		level.resize(N+10);
-	}
+struct NetworkFlow{  // use Dinic
+
+	using FlowType = int;
+
+	struct Edge{ int to, rev; FlowType cap; };
+	vector<Edge> graph[SZ];
+	int level[SZ], work[SZ];
 
 	// 마지막 인자를 안쓰면 유방향, cap과 같게 쓰면 무방향(양쪽 cap 같음)
-	void add_edge(int from, int to, int cap, int caprev = 0) {
-		graph[from].emplace_back(from, to, cap);
-		graph[to].emplace_back(to, from, caprev);
-		graph[from].back().rev = graph[to].size() - 1;
-		graph[to].back().rev = graph[from].size() - 1;
+	void addEdge(int _from, int _to, FlowType _cap, FlowType _caprev = 0){
+		graph[_from].push_back({_to, (int)graph[_to].size(), _cap});
+		graph[_to].push_back({_from, (int)graph[_from].size()-1, -_caprev});
 	}
-	void add_edge_from_source(int to, int cap) {
-		add_edge(SRC, to, cap);
+
+	void initGraph(){ // 테스트케이스를 위한 그래프 초기화
+		for (int i=0; i<SZ; i++) graph[i].clear();
 	}
-	void add_edge_to_sink(int from, int cap) {
-		add_edge(from, SINK, cap);
-	}
-	int dfs(int c, int minFlow = INT_MAX) {
-		if (c == SINK) {
-			return minFlow;
-		}
-		int flow;
-		for (int &i = work[c]; i < graph[c].size(); i++) {
-			auto &e = graph[c][i];
-			if (e.cap > 0 && level[e.to] == level[e.from] + 1 && (flow = dfs(e.to, min(e.cap, minFlow)))) {
-				e.cap -= flow;
-				graph[e.to][e.rev].cap += flow;
-				return flow;
-			}
-		}
-		return 0;
-	}
-	int maxFlow() {
-		int ret = 0;
-		queue<int> q;
-		while(true) {
-			int flow = 0;
-			fill(level.begin(), level.end(), -1);
-			q.push(SRC);
-			level[SRC] = 0;
-			while (!q.empty()) {
-				int c = q.front();
-				q.pop();
-				for (auto &e : graph[c]) {
-					if (e.cap > 0 && level[e.to] == -1) {
-						level[e.to] = level[e.from] + 1;
-						q.push(e.to);
-					}
-				}
-			}
-			fill(work.begin(), work.end(), 0);
-			while (int temp = dfs(SRC)) {
-				flow += temp;
-			}
-			if (flow == 0) {
-				break;
-			}
-			ret += flow;
-		}
-		return ret;
-	}
-};
+
+    bool BFS(int SRC, int SINK){
+        memset(level, 0, sizeof(level));
+        queue<int> q; q.push(SRC); level[SRC] = 1;
+        while(!q.empty()){
+            int now = q.front(); q.pop();
+            for(const auto &next : graph[now]){
+                if(!level[next.to] && next.cap) q.push(next.to), level[next.to] = level[now] + 1;
+            }
+        }
+        return level[SINK];
+    }
+    FlowType DFS(int now, int SINK, FlowType flow){
+        if(now == SINK) return flow;
+		for(; work[now] < (int)graph[now].size(); work[now]++){
+            auto &next = graph[now][work[now]];
+            if(level[next.to] != level[now] + 1 || !next.cap) continue;
+            FlowType ret = DFS(next.to, SINK, min(flow, next.cap));
+            if(!ret) continue;
+            next.cap -= ret;
+            graph[next.to][next.rev].cap += ret;
+            return ret;
+        }
+        return 0;
+    }
+    FlowType maxFlow(int s = SRC, int t = SINK){
+        FlowType ret = 0, minFlow;
+        while(BFS(s, t)){
+            memset(work, 0, sizeof(work));
+            while((minFlow = DFS(s, t, INF))) ret += minFlow;
+        }
+        return ret;
+    }
+} nf;
 
 void solve(){
+
+	nf.initGraph();
+
     int n, m;
     cin >> n >> m;
-
-    MaximumFlow ff(2500, 2501, 2502);
 
     int totalSum = 0;
     bool isRed = true;
@@ -115,10 +96,15 @@ void solve(){
             cin >> cap;
             totalSum+=cap;
 
+			// Blue : Sink 연결
+            if ((r + c) % 2) {
+				nf.addEdge(order, SINK, cap);
+			}
+
             // red : Source 연결
             // 상하좌우 Blue와 연결
-            if (isRed){
-                ff.add_edge_from_source(order, cap);
+        	else {
+                nf.addEdge(SRC, order, cap);
                 for(int i=0; i<4 ; ++i){
                     int nr = r + dx[i];
                     int nc = c + dy[i]; 
@@ -127,22 +113,14 @@ void solve(){
                     if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
                     
                     int blue_order = order + change_order[i];
-                    ff.add_edge(order, blue_order, INF);
+                    nf.addEdge(order, blue_order, INF);
                 }
             }
-            // Blue : Sink 연결
-            else {
-                ff.add_edge_to_sink(order, cap);
-            }
-
             order++;
-            isRed^=1;
         }
-        // 열이 짝수면 다시 색변경
-        if (m%2==0) isRed^=1;
     }
 
-    cout << totalSum - ff.maxFlow() << '\n';
+    cout << totalSum - nf.maxFlow() << '\n';
 }
 
 int main(){
