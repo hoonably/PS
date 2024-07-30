b35a298ee988fd37714475e3794910a3ec131deb
hoonably <77783081+hoonably@users.noreply.github.com>
2024-07-30 20:13:52 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 136 ms, Memory: 26228 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -1,67 +1,78 @@
 #include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
-typedef pair<int,int> pii;
-#define MAX 1'000'001
-#define MOD 1'000'000'007
+typedef pair<int,int> pii; typedef pair<ll,ll> pll; 
+typedef tuple<int,int,int> tiii;
+const int INF = 0x3f3f3f3f;  // 1061109567
+// const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1'000'000'007;
 
-/*
-세그먼트 트리
-구간 곱 구해서 MOD로 나눈 나머지 구하기
+/* -----------------------------------------------------
+구간 곱 세그먼트 트리
+https://www.acmicpc.net/problem/11505
 */
 
-int N, M, K;
-ll arr[MAX];
-vector<ll> segTree;
+#define MAX 1'000'000
 
-ll init(int node, int start, int end) {
-    if (start == end)
-        return segTree[node] = arr[start];
-    int mid = (start + end) / 2;
-    return segTree[node] = init(node*2, start, mid) 
-    * init(node*2 + 1, mid + 1, end) % MOD;
-}
+ll arr[MAX+1];  // 미리 arr를 정해놓는 경우
 
-void update(int node, int start, int end, int idx, ll value){
-    if (idx < start || end < idx) return;
- 
-    if (start == end){
-        segTree[node] = value;
-        return;
+struct SegmentTree {
+
+    using DataType = long long;
+    vector<DataType> tree;
+
+    SegmentTree(int size) {
+        int h = ceil(log2(size+1));  // ceil : 정수로 올림
+        int treeSize = 1 << (h + 1);
+        tree.resize(treeSize);
     }
- 
-    int mid = (start + end) / 2;
-    update(node * 2, start, mid, idx, value);
-    update(node * 2 + 1, mid + 1, end, idx, value);
-    segTree[node] = (segTree[node * 2] * segTree[node * 2 + 1]) % MOD;
-}
 
-ll multiple(int node, int start, int end, int left, int right){
-    if (left > end || right < start)
-        return 1;  // 곱이기 때문에
-    if (left <= start && right >= end)
-        return segTree[node];
-    int mid = (start + end) / 2;
-    return multiple(node*2, start, mid, left, right) 
-    * multiple(node*2 + 1, mid + 1, end, left, right) % MOD;
-}
+    // 미리 정해놓은 배열로 세그트리 구성
+    DataType build(int node, int s, int e) {  // 인덱스 따라서 (1, 1, N) / (0, 0, N-1)
+        if (s == e)
+            return tree[node] = arr[s];
+        int mid = (s + e) / 2;
+        return tree[node] = build(node*2, s, mid) * build(node*2 + 1, mid + 1, e) % MOD;
+    }
+
+    // idx 인덱스의 숫자를 value "로" 변화
+    void update(int node, int s, int e, int idx, DataType value){
+        if (idx > e || idx < s) return;
+        if (s == e) {
+            tree[node] = value; return;
+        }
+        int mid = (s + e) / 2;
+        update(node*2, s, mid, idx, value);
+        update(node*2 + 1, mid + 1, e, idx, value);
+        tree[node] = tree[node * 2] * tree[node * 2 + 1] % MOD;
+    }
+
+    // left~right 구간곱 구하기
+    DataType multiple(int node, int s, int e, int left, int right){
+        if (left > e || right < s)
+            return 1;  // 곱이기 때문에
+        if (left <= s && right >= e)
+            return tree[node];
+        int mid = (s + e) / 2;
+        return multiple(node*2, s, mid, left, right) 
+        * multiple(node*2 + 1, mid + 1, e, left, right) % MOD;
+    }
+}ST(MAX);
+ 
+int N, M, K;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     
     cin >> N >> M >> K;
 
-    // 트리의 크기
-    int h = ceil(log2(N+1));  // ceil : 정수로 올림
-    int treeSize = 1 << (h + 1);
-    segTree = vector<ll>(treeSize);
-
     for (int i=1; i<=N; i++){
         cin >> arr[i];
     }
 
     // 트리 생성
-    init(1, 1, N);
+    ST.build(1, 1, N);
 
     for (int i=0; i<M+K; i++){
         ll a, b, c;
@@ -70,11 +81,11 @@ int main(){
         // b번째 수를 c로 변경
         if (a==1){
             arr[b] = c;
-            update(1, 1, N, b, c); 
+            ST.update(1, 1, N, b, c); 
         }
         // b번째수 ~ C번째 수 곱 구하기
         else {
-            cout << multiple(1, 1, N, b, c) << "\n";
+            cout << ST.multiple(1, 1, N, b, c) << "\n";
         }
     }
     
