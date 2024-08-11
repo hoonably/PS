ee20f68788190f0a258a75d012085aefbecb6278
hoonably <77783081+hoonably@users.noreply.github.com>
2024-08-11 21:28:47 +0900

    [Platinum II] Title: 서로 다른 수와 쿼리 1, Time: 3292 ms, Memory: 29388 KB -BaekjoonHub

--- 백준/Platinum/14897. 서로 다른 수와 쿼리 1/서로 다른 수와 쿼리 1.cc
+++ 백준/Platinum/14897. 서로 다른 수와 쿼리 1/서로 다른 수와 쿼리 1.cc
@@ -9,17 +9,9 @@ const int INF = 0x3f3f3f3f;  // 1061109567
 // const int MOD = 1'000'000'007;
 
 /* -----------------------------------------------------
-https://www.acmicpc.net/problem/13547
+https://www.acmicpc.net/problem/14897
 
-평행 분할(SQRT Decomposition) : 데이터가 N개이면 √N개만큼 나눠서 관리하는 방법
-
-모스 알고리즘은 어떤 구간 [s, e]에 속하는 원소들을 이용하여 어떤 값을 계산하는 쿼리를 처리하는 알고리즘이다.
-
-업데이트가 없어 쿼리의 순서를 변경해도 무방하기 때문에 쿼리 순서를 재배치하여 효율적으로 수행
-
-배열을 k(√N)개로 이루어진 그룹으로 나눠주고, 쿼리를 실행할 때 두 조건 중 하나를 만족하는 경우 먼저 처리한다.
-[s1/k] < [s2/k]
-[s1/k] = [s2/k] and e1 < e2
+평행분할 + mo's 알고리즘 + 좌표압축
 */
 
 #define MAX 1'000'001
@@ -55,7 +47,7 @@ int main(){
     B = A;
     sort(B.begin(), B.end());
     B.erase(unique(B.begin(), B.end()), B.end());
-    for(int i=1; i<=N; i++) {
+    for(int i=1; i<=N; ++i) {
         A[i] = lower_bound(B.begin(), B.end(), A[i]) - B.begin();
     }
 
@@ -67,10 +59,9 @@ int main(){
     }
     sort(all(Qu));  // 쿼리 정렬
 
-    int s=0, e=0;
-
     // 이전 쿼리의 결과를 이용해 계산해나가기
-    for(int i=0;i<Q;++i){
+    int s=0, e=0;
+    for(int i=0; i<Q; ++i){
         while(s<Qu[i].s) if (--cnt[A[s++]]==0) --res;
         while(s>Qu[i].s) if (++cnt[A[--s]]==1) ++res;
         while(e>Qu[i].e) if (--cnt[A[e--]]==0) --res;
@@ -78,7 +69,7 @@ int main(){
         ans[Qu[i].idx]=res;
     }
 
-    for(int i=0; i<Q; i++) {
+    for(int i=0; i<Q; ++i) {
         cout << ans[i] << '\n';
     }
     
