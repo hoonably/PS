95ee62c4e209dae9e9722e7a6d462a0e43f6aad0
hoonably <77783081+hoonably@users.noreply.github.com>
2024-08-18 01:38:39 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 0 ms, Memory: 3312 KB -BaekjoonHub

--- 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
+++ 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -9,6 +9,7 @@ const int INF = 0x3f3f3f3f;  // 1061109567
 // const int MOD = 1'000'000'007;
 
 /* -----------------------------------------------------
+https://www.acmicpc.net/problem/13275
 
 Manacher's Algorithm
 문자열의 부분 문자열이 팰린드롬인지 O(n)에 판단하는 manacher 알고리즘
@@ -32,15 +33,23 @@ i - A[i] - 1에서 i + A[i] + 1까지는 팰린드롬이  아니다.
 
 int A[2*MAX+2];
  
-void manachers(string S, int len){
+void manachers(const string& S, int len){
     int r = 0, p = 0;
     for (int i = 0; i < len; i++){
+        //   i ≤ r이라면, i는 p를 중심으로 한 회문에 속한다. 따라서 그 회문에서 i의 대칭점을 i′라 하자. 즉, i′=2p - i가 된다.
+        //  그리고 A[i]의 초기값은 min(r - i, A[i′])이다. (즉, r은 중심점 p를 기준으로 하는 p + A[p]를 의미하게 된다.)
         if (i <= r) A[i] = min(A[2 * p - i], r - i);
+
+        //  i > r이라면, A[i]의 초기값은 0이다.
         else A[i] = 0;
- 
-        while (i - A[i] - 1 >= 0 && i + A[i] + 1 < len && S[i - A[i] - 1] == S[i + A[i] + 1])
-            A[i]++;
- 
+
+        // S[i - A[i]]와 S[i + A[i]]가 같을 때까지 A[i]를 증가시킨다.
+        while (i - A[i] - 1 >= 0 && i + A[i] + 1 < len){
+            if (S[i - A[i] - 1] == S[i + A[i] + 1]) A[i]++;
+            else break;
+        }
+
+        // 갱신
         if (r < i + A[i]) {
             r = i + A[i];
             p = i;
@@ -59,6 +68,8 @@ int main(){
 
     str.resize(2*len+1);
 
+    // 사이사이 # 넣어주기
+    // #b#a#n#a#n#a#
     str[2 * len] = '#';
     for (int i = len - 1; i >= 0; i--) {
         str[i * 2 + 1] = str[i];
