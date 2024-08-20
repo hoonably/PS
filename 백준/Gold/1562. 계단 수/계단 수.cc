4df72c724eb86aec484d3816b66dcd6e4b804de6
hoonably <77783081+hoonably@users.noreply.github.com>
2024-08-21 08:34:54 +0900

    [Gold I] Title: 계단 수, Time: 4 ms, Memory: 6060 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,ll> pll; 
+typedef tuple<int,int,int> tiii;
+const int INF = 0x3f3f3f3f;  // 1061109567
+// const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1000000000;
+
+/* -----------------------------------------------------
+https://www.acmicpc.net/problem/1562
+*/
+
+#define MAX 
+
+int n;
+int dp[101][10][1024];	
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> n;
+	//base
+	dp[1][0][1] = 0;	//명시적
+	dp[1][1][2] = 1;
+	dp[1][2][4] = 1;
+	dp[1][3][8] = 1;
+	dp[1][4][16] = 1;
+	dp[1][5][32] = 1;
+	dp[1][6][64] = 1;
+	dp[1][7][128] = 1;
+	dp[1][8][256] = 1;
+	dp[1][9][512] = 1;
+ 
+	//step
+	for (int i = 2; i <= n; ++i) {
+
+		// cur = 0
+        int cur = 0;
+        for (int state = 0; state <= 1023; ++state) 
+            dp[i][cur][state | (1 << cur)] = (dp[i][cur][state | (1 << cur)] + dp[i - 1][cur + 1][state])%MOD;
+        
+        // cur 1~8
+		while(++cur<9) {
+			for (int state = 0; state <= 1023; ++state) {
+					dp[i][cur][state | (1 << cur)] = (dp[i][cur][state | (1 << cur)] + dp[i - 1][cur + 1][state]) % MOD;
+					dp[i][cur][state | (1 << cur)] = (dp[i][cur][state | (1 << cur)] + dp[i - 1][cur - 1][state]) % MOD;
+			}
+		}
+
+        // cur 9
+        cur = 9;
+        for (int state = 0; state <= 1023; ++state)
+            dp[i][cur][state | (1 << cur)] = (dp[i][cur][state | (1 << cur)] + dp[i - 1][cur - 1][state]) % MOD;
+    }
+ 
+	int ans = 0;
+	for (int cur = 0; cur <= 9; ++cur) ans = (ans + dp[n][cur][1023]) % MOD;
+
+    cout << ans;
+    
+    return 0;
+}
