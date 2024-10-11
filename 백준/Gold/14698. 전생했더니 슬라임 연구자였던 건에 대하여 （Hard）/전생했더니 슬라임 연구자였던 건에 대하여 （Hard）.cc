ff85e8ffa0e8c0e6d3b40ffbf97b03721fb0e618
Jeonghoon Park <77783081+hoonably@users.noreply.github.com>
2024-10-11 14:24:51 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 160 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,ll> pll; 
+typedef tuple<int,int,int> tiii;
+const int INF = 0x3f3f3f3f;  // 1061109567
+// const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+/* -----------------------------------------------------
+
+*/
+
+#define MAX 
+
+
+
+void solve(){
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    int n; cin >> n;
+    for(int i=0; i<n; i++){
+        ll x; cin >> x;
+        pq.push(x);
+    }
+
+    ll ans = 1;
+    while(pq.size()>=2){
+        ll small1 = pq.top(); pq.pop();
+        ll small2 = pq.top(); pq.pop();
+        ll big = small1 * small2;
+        pq.push(big);
+        ans *= big % MOD;
+        ans %= MOD;
+    }
+    cout << ans << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();   
+    
+    return 0;
+}
