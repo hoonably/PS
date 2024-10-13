8e342306bb8147094467425a847ab1b7caaaa346
Jeonghoon Park <77783081+hoonably@users.noreply.github.com>
2024-10-13 21:50:26 +0900

    [Gold I] Title: 효구와 호규 (Hard), Time: 152 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Gold/26097. 효구와 호규 （Hard）/효구와 호규 （Hard）.cc
+++ 백준/Gold/26097. 효구와 호규 （Hard）/효구와 호규 （Hard）.cc
@@ -15,28 +15,28 @@ const int INF = 0x3f3f3f3f;  // 1061109567
 #define MAX 
 
 int N, M;
-char board[1001][1001];
+char board[1002][1002];
 int sum;
 
 void print(){
     // 0 두개 지우기
     bool flag = 1;
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= M; j++) {
             if (board[i][j] == '0') {
                 board[i][j] = '2';
-                cout << i+1 << ' ' << j+1 << ' ';
+                cout << i << ' ' << j << ' ';
                 flag ^= 1;
                 if (flag) cout << '\n';  // 두개의 0을 지웠을 때만 줄바꿈
             }
         }
     }
     // 1 두개 지우기
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= M; j++) {
             if (board[i][j] == '1') {
                 board[i][j] = '2';
-                cout << i+1 << ' ' << j+1 << ' ';
+                cout << i << ' ' << j << ' ';
                 flag ^= 1;
                 if (flag) cout << '\n';  // 두개의 1을 지웠을 때만 줄바꿈
             }
@@ -51,20 +51,19 @@ int main(){
     
     cin >> N >> M;
 
-    // 홀수개면 불가능
+    // 총 홀수개면 불가능
     if (N%2 && M%2) {
         cout << "-1";
         return 0;
     }
 
-    int a;
-    memset(board, -1, sizeof(board));
-    for(int i = 0; i < N; i++) for(int j = 0; j < M; j++) {
+    memset(board, 'x', sizeof(board));
+    for(int i = 1; i <= N; i++) for(int j = 1; j <= M; j++) {
         cin >> board[i][j];
         sum += (board[i][j] == '1');
     }
 
-    // 1의 개수가 홀수인가?
+    // 1과 0의 개수가 홀수인가?
     if (sum % 2) {
         cout << "-1";
         return 0;
@@ -72,22 +71,20 @@ int main(){
 
     // 처음에 한번이라도 없어지면 가능
     // 자신의 오른쪽과 아래에 있는 것을 비교
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
-            if (board[i][j] == board[i+1][j]) {
-                cout << "1\n";
-                board[i][j] = board[i+1][j] = '2';
-                cout << i+1 << ' ' << j+1 << ' ' << i+2 << ' ' << j+1 << '\n';
-                print();
-                return 0;
-            }
-            if (board[i][j] == board[i][j+1]) {
-                cout << "1\n";
-                board[i][j] = board[i][j+1] = '2';
-                cout << i+1 << ' ' << j+1 << ' ' << i+1 << ' ' << j+2 << '\n';
-                print();
-                return 0;
-            }
+    for(int i = 1; i <= N; i++) for(int j = 1; j <= M; j++) {
+        if (board[i][j] == board[i+1][j]) {
+            cout << "1\n";
+            board[i][j] = board[i+1][j] = '2';
+            cout << i << ' ' << j << ' ' << i+1 << ' ' << j << '\n';
+            print();
+            return 0;
+        }
+        if (board[i][j] == board[i][j+1]) {
+            cout << "1\n";
+            board[i][j] = board[i][j+1] = '2';
+            cout << i << ' ' << j << ' ' << i << ' ' << j+1 << '\n';
+            print();
+            return 0;
         }
     }
 
