dc02f2e9f5e8a5329270146ea86ebf47120aea9a
Jeonghoon Park <happypjh2001@unist.ac.kr>
2024-10-24 13:53:47 +0900

    [Silver IV] Title: 크림 파스타, Time: 24 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.cc
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.cc
@@ -14,18 +14,36 @@ const int INF = 0x3f3f3f3f;  // 1061109567
 
 #define MAX 
 
+char rbuf[1<<16];
+int Ridx, Rridx;
+inline char read(){
+    if(Rridx == Ridx){
+        Rridx = fread(rbuf, 1, 1<<16, stdin);
+        if(!Rridx) return 0;
+        Ridx = 0;
+    }
+    return rbuf[Ridx++];
+}
+inline int readInt(){
+    int sum = 0;
+    char now = read();
+    while(now <= 32) now = read();
+    while(now >= 48) sum = sum * 10 + now - '0', now = read();
+    return sum;
+}
+
 int N;
 int ans;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     
-    cin >> N;
+    N = readInt();
 
     int mini = 1e9;
     int now;
     for(int i=0; i<N; i++){
-        cin >> now;
+        now = readInt();
         mini = min(mini, now);
         ans = max(ans, now-mini);
         cout << ans << ' ';
