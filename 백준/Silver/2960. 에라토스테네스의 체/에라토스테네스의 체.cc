600946fe1131d8fdc42ff6d243616a49cbcb9c8c
Jeonghoon Park <happypjh2001@unist.ac.kr>
2024-11-14 11:44:10 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+const int INF = 0x3f3f3f3f;  // 1061109567
+// const ll INF = 0x3f3f3f3f3f3f3f3f;
+// const int MOD = 1000000007;
+
+/* -----------------------------------------------------
+
+*/
+
+int N, K;
+int cnt;
+
+int eratos() {
+    vector<bool> isPrime(N + 1, true);
+    
+    for (int i = 2; i<= N; ++i) {
+        if (isPrime[i]) {
+            for (int j = i; j <= N; j += i) {
+                if (isPrime[j]) {
+                    isPrime[j] = false;
+                    if (++cnt==K) return j;
+                    // cout << j << ' ';
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    cin >> N >> K;
+    cout << eratos();
+    
+    return 0;
+}
