50eb30117928452c8dc9e56cb0f14dd8d61e0492
Jeonghoon Park <happypjh2001@unist.ac.kr>
2024-11-22 13:05:48 +0900

    [Silver IV] Title: 링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3036. 링/링.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+const int INF = 0x3f3f3f3f;  // 1061109567
+// const ll INF = 0x3f3f3f3f3f3f3f3f;
+// const int MOD = 1000000007;
+
+/* -----------------------------------------------------
+
+*/
+
+int N;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int N; cin >> N;
+    
+    int first; cin >> first;
+    for(int i=1; i<N; i++){
+        int x; cin >> x;
+        int a = first, b = x;
+        while(b){
+            int r = a%b;
+            a = b;
+            b = r;
+        }
+        cout << first/a << '/' << x/a << '\n';
+    }
+
+    
+    return 0;
+}
